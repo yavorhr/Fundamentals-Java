572f495fdb9e7650829daf731c6d878e2c3a3be0
wolfie <yavorhristozov@gmail.com>
2023-08-19 16:11:21 +0300

    Refactor code

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/03. Passed or Failed/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/03. Passed or Failed/Main.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+    static final double BOUND_GRADE = 2.99;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double grade = Double.parseDouble(scanner.nextLine());
-
-        if (grade >= 3.00) {
-            System.out.println("Passed!");
-        } else {
-            System.out.println("Failed!");
-        }
-
+        String result = getResultIfGradePass(scanner);
+        System.out.println(result);
     }
+
+    private static String getResultIfGradePass(Scanner scanner) {
+        double grade = Double.parseDouble(scanner.nextLine());
+        String result = "";
+
+        if (grade > BOUND_GRADE) {
+            result = "Passed!";
+        } else {
+            result = "Failed!";
+        }
+        return result;
+    }
+
 }
