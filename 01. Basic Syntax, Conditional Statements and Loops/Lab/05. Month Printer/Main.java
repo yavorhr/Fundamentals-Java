2a1ca7f95c21f1dd1f3a8e2b05b4c51237c63a83
wolfie <yavorhristozov@gmail.com>
2023-08-19 16:42:34 +0300

    Refactor 05. Month Printer
    practice

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/05. Month Printer/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/05. Month Printer/Main.java
@@ -5,47 +5,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int month = Integer.parseInt(scanner.nextLine());
+        String result = getMonth(month);
 
-        switch (month) {
-            case 1:
-                System.out.println("January");
-                break;
-            case 2:
-                System.out.println("February");
-                break;
-            case 3:
-                System.out.println("March");
-                break;
-            case 4:
-                System.out.println("April");
-                break;
-            case 5:
-                System.out.println("May");
-                break;
-            case 6:
-                System.out.println("June");
-                break;
-            case 7:
-                System.out.println("July");
-                break;
-            case 8:
-                System.out.println("August");
-                break;
-            case 9:
-                System.out.println("September");
-                break;
-            case 10:
-                System.out.println("October");
-                break;
-            case 11:
-                System.out.println("November");
-                break;
-            case 12:
-                System.out.println("December");
-                break;
-            default:
-                System.out.println("Error!");
+        System.out.println(result);
+    }
+
+    private static String getMonth(int monthAsNumber) {
+        String result = "";
+
+        switch (monthAsNumber) {
+            case 1 -> result = "January";
+            case 2 -> result = "February";
+            case 3 -> result = "March";
+            case 4 -> result = "April";
+            case 5 -> result = "May";
+            case 6 -> result = "June";
+            case 7 -> result = "July";
+            case 8 -> result = "August";
+            case 9 -> result = "September";
+            case 10 -> result = "October";
+            case 11 -> result = "November";
+            case 12 -> result = "December";
+            default -> result = "Error!";
         }
+
+        return result;
 
     }
 }
