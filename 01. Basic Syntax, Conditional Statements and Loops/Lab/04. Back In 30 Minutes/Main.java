d183efc01952985f73a8c03db119b8b0235f4126
wolfie <yavorhristozov@gmail.com>
2023-08-19 16:31:21 +0300

    Refactor 04. Back in 30 Minutes

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/04. Back In 30 Minutes/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/04. Back In 30 Minutes/Main.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final int TIME_IN_30_MINUTES = 30;
+    private static final int MINUTES_IN_1_HOUR = 60;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int hours = Integer.parseInt(scanner.nextLine());
-        int minutes = Integer.parseInt(scanner.nextLine());
+        int readHours = Integer.parseInt(scanner.nextLine());
+        int readMinutes = Integer.parseInt(scanner.nextLine());
 
-        int allMinutes = (hours * 60) + minutes + 30;
+        int totalMinutes = getTotalMinutes(readHours, readMinutes);
 
-        int hour = allMinutes / 60;
+        int minutes = totalMinutes % 60;
+        int hours = totalMinutes / 60;
 
-        if (hour == 24) {
-            hour = 0;
+        if (hours == 24) {
+            hours = 0;
         }
 
-        int minute = allMinutes % 60;
-        System.out.printf("%d:%02d", hour, minute);
+        System.out.printf("%d:%02d", hours, minutes);
+    }
 
+    private static int getTotalMinutes(int hours, int minutes) {
+        int convertHoursIntoMinutes = hours * MINUTES_IN_1_HOUR;
+        return convertHoursIntoMinutes + minutes + TIME_IN_30_MINUTES;
     }
 }
-
