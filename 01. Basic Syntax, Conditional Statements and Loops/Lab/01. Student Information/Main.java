215bae26cc4adf7956edfde458ecd82e0f5128bf
wolfie <yavor.hristozov@gmail.com>
2023-08-19 15:56:04 +0300

    Refact

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/01. Student Information/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/01. Student Information/Main.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+    static final double BOUND_GRADE = 3.00;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String name = scanner.nextLine();
-        int age = Integer.parseInt(scanner.nextLine());
-        double grade = Double.parseDouble(scanner.nextLine());
+        String result = getResultIfGradePass(scanner);
 
-        System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
+        System.out.println(result);
     }
+
+    private static String getResultIfGradePass(Scanner scanner) {
+        double grade = Double.parseDouble(scanner.nextLine());
+        String result = "";
+
+        if (grade >= BOUND_GRADE) {
+            result = "Passed!";
+        }
+        return result;
+    }
+
 }
+
