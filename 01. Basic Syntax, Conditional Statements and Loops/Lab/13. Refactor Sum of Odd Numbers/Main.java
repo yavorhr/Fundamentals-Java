6e1bbb74f813459fc6c03dca29db84bc54acddc5
wolfie <yavorhristozov@gmail.com>
2023-08-20 13:49:09 +0300

    refact : 13. Refactor Sum of Odd Numbers

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/13. Refactor Sum of Odd Numbers/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/13. Refactor Sum of Odd Numbers/Main.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int sum = 0;
-        int n = Integer.parseInt(sc.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+        int oddSum = 0;
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(2 * i + 1);
-            sum += 2 * i + 1;
+        for (int i = 1; i <= n; i++) {
+            int oddNumber = 2 * i - 1;
+            System.out.println(oddNumber);
+            oddSum += oddNumber;
         }
-        System.out.printf("Sum: %d%n", sum);
 
+        System.out.printf("Sum: %d%n", oddSum);
     }
 }
