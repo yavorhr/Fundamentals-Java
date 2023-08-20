020ed290b2d0b19b3f68189d4404cd6907c8e4a1
wolfie <yavorhristozov@gmail.com>
2023-08-20 13:44:53 +0300

    refact : 12. Even Number

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/12. Even Number/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/12. Even Number/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
-        while (n % 2 != 0) {
-            System.out.println("Please write an even number.");
-            n = Integer.parseInt(scanner.nextLine());
+        int inputNumber = Integer.parseInt(scanner.nextLine());
+
+        while (inputNumber % 2 != 0) {
+            System.out.println("Please write an even number");
+            inputNumber = Integer.parseInt(scanner.nextLine());
         }
-        System.out.printf("The number is: %d", Math.abs(n));
+
+        System.out.println("The number is: " + Math.abs(inputNumber));
     }
-
 }
-
