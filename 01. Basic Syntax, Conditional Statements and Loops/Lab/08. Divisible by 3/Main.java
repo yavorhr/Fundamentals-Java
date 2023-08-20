c64d5a0a0273ab60502cfedd078cbbade1c23ee8
wolfie <yavorhristozov@gmail.com>
2023-08-20 12:31:54 +0300

    refact : 08. Divisible by 3

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/08. Divisible by 3/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/08. Divisible by 3/Main.java
@@ -1,10 +1,10 @@
-
 public class Main {
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 100;
     public static void main(String[] args) {
 
-        for (int i = 3; i <= 100; i += 3) {
+        for (int i = START_INDEX; i < END_INDEX; i+=3) {
             System.out.println(i);
         }
-
     }
 }
