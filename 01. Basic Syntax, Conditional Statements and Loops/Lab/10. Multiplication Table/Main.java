6669b0310714897b9cad57d908ce47f3e8809eb5
wolfie <yavorhristozov@gmail.com>
2023-08-20 13:21:24 +0300

    refact : 10. Multiplication table

--- 01. Basic Syntax, Conditional Statements and Loops/Lab/10. Multiplication Table/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/10. Multiplication Table/Main.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 10;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        int number = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= 10; i++) {
-            int formula = n * i;
-            System.out.printf("%d X %d = %d%n", n, i, formula);
+        for (int i = START_NUMBER; i <= END_NUMBER; i++) {
+            System.out.printf("%d X %d = %d%n", number, i, number * i);
         }
-
     }
 }
