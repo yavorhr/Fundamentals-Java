2a78516c68ea20857761ed7a3d32355780580bd8
wolfie <yavorhristozov@gmail.com>
2023-08-21 13:22:33 +0300

    refact : 05. Print sum
    Split code in Main using methods
    add try catch for the user's input

--- 01. Basic Syntax, Conditional Statements and Loops/Exercise/04. Print and Sum/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Exercise/04. Print and Sum/Main.java
@@ -4,17 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int startNumber = Integer.parseInt(scanner.nextLine());
-        int endNumber = Integer.parseInt(scanner.nextLine());
+        try {
+            int startNumber = Integer.parseInt(scanner.nextLine());
+            int endNumber = Integer.parseInt(scanner.nextLine());
 
+            int sum = sumNumbers(startNumber, endNumber);
+            printResult(sum);
+        } catch (NumberFormatException e){
+            System.out.println("Please insert valid int input");
+        }
+    }
+
+    private static int sumNumbers(int startNumber, int endNumber) {
         int sum = 0;
 
         for (int i = startNumber; i <= endNumber; i++) {
             System.out.print(i + " ");
             sum += i;
         }
-        
+        return sum;
+    }
+
+    private static void printResult(int sum) {
         System.out.println();
-        System.out.println("Sum: " + sum);
+        System.out.printf("Sum: %d", sum);
     }
 }
