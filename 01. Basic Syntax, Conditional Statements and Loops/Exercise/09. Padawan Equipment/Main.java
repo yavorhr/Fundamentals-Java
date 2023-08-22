b0ad751d8ee80731af11e50079a4b16a2e8ed09b
wolfie <yavorhristozov@gmail.com>
2023-08-22 12:13:15 +0300

    refact : 9. Padawan Equipment
    split Main class by extracting it's code into separate methods

--- 01. Basic Syntax, Conditional Statements and Loops/Exercise/09. Padawan Equipment/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Exercise/09. Padawan Equipment/Main.java
@@ -4,24 +4,51 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double budget = Double.parseDouble(scanner.nextLine());
-        int students = Integer.parseInt(scanner.nextLine());
-        double priceSabre = Double.parseDouble(scanner.nextLine());     //total sabres + 10%
-        double priceRobes = Double.parseDouble(scanner.nextLine());
-        double priceBelt = Double.parseDouble(scanner.nextLine());      //every 6 is free
+        double budget = readDoubleFromConsole(scanner);
+        int studentsCount = Integer.parseInt(scanner.nextLine());
+        double lightSaberPrice = readDoubleFromConsole(scanner);
+        double robePrice = readDoubleFromConsole(scanner);
+        double beltPrice = readDoubleFromConsole(scanner);
 
-        int freeBelt = students / 6;
-        double belts = (priceBelt * students) - freeBelt * priceBelt;
-        double sabres = (Math.ceil(students * 1.1)) * priceSabre;
-        double robes = priceRobes * students;
+        double totalAmount = calculateTotalAmount(studentsCount, lightSaberPrice, robePrice, beltPrice);
+        printResult(budget, totalAmount);
+    }
 
-        double costs = belts + sabres + robes;
-
-        if (budget >= costs) {
-            System.out.printf("The money is enough - it would cost %.2flv.", costs);
+    private static void printResult(double budget, double totalAmount) {
+        if (budget >= totalAmount) {
+            System.out.printf("The money is enough - it would cost %.2flv.", totalAmount);
         } else {
-            System.out.printf("Ivan Cho will need %.2flv more.", costs - budget);
+            System.out.printf("Ivan Cho will need %.2flv more.", Math.abs(totalAmount - budget));
         }
+    }
 
+    private static double calculateTotalAmount(int studentsCount, double lightSaberPrice, double robePrice, double beltPrice) {
+        double beltsAmount = calculateBeltsAmount(studentsCount, beltPrice);
+        double lightSabersAmount = calculateLightSabersAmount(studentsCount, lightSaberPrice);
+        double robesAmount = calculateRobesAmount(studentsCount, robePrice);
+
+        return beltsAmount + lightSabersAmount + robesAmount;
+    }
+
+    private static double calculateRobesAmount(int studentsCount, double robePrice) {
+        return studentsCount * robePrice;
+    }
+
+    private static double calculateBeltsAmount(int studentsCount, double beltPrice) {
+        double beltsTotalAmount = studentsCount * beltPrice;
+        int beltsCount = studentsCount / 6;
+        if (beltsCount > 0) {
+            beltsTotalAmount -= beltsCount * beltPrice;
+        }
+        return beltsTotalAmount;
+    }
+
+    private static double calculateLightSabersAmount(int studentsCount, double lightSaberPrice) {
+        double additionalPercent = Math.ceil(studentsCount + studentsCount * 0.1);
+        return additionalPercent * lightSaberPrice;
+    }
+
+    private static double readDoubleFromConsole(Scanner scanner) {
+        return Double.parseDouble(scanner.nextLine());
     }
 }
