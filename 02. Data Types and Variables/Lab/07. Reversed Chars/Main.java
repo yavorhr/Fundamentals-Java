c5d7a456a0b3d23a7740ac546767649892b4e810
wolfie <yavorhristozov@gmail.com>
2023-08-22 18:05:11 +0300

    refact : 07. Reversed Chars

--- 02. Data Types and Variables/Lab/07. Reversed Chars/Main.java
+++ 02. Data Types and Variables/Lab/07. Reversed Chars/Main.java
@@ -4,10 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        char firstChar = scanner.nextLine().charAt(0);
-        char secondChar = scanner.nextLine().charAt(0);
-        char thirdChar = scanner.nextLine().charAt(0);
+        char firstChar = readCharFromConsole(scanner);
+        char secondChar = readCharFromConsole(scanner);
+        char thirdChar = readCharFromConsole(scanner);
 
-        System.out.printf("%c %c %c", thirdChar, secondChar, firstChar);
+        printReversedOutput(thirdChar, secondChar, firstChar);
+    }
+
+    private static char readCharFromConsole(Scanner scanner) {
+        return scanner.nextLine().charAt(0);
+    }
+
+    private static void printReversedOutput(char firstChar, char secondChar, char thirdChar) {
+        System.out.printf("%c %c %c", firstChar, secondChar, thirdChar);
     }
 }
