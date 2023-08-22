7e9e4a58cfdb91388b6977a7303de5e4181b2931
wolfie <yavorhristozov@gmail.com>
2023-08-22 12:44:25 +0300

    refact : 10. Rage Expenses
    split Main code by extracting simple methods

--- 01. Basic Syntax, Conditional Statements and Loops/Exercise/10. Rage Expenses/Main.java
+++ 01. Basic Syntax, Conditional Statements and Loops/Exercise/10. Rage Expenses/Main.java
@@ -5,26 +5,49 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int lostGamesCount = Integer.parseInt(scanner.nextLine());
-        double headsetPrice = Double.parseDouble(scanner.nextLine());
-        double mousePrice = Double.parseDouble(scanner.nextLine());
-        double keyboardPrice = Double.parseDouble(scanner.nextLine());
-        double displayPrice = Double.parseDouble(scanner.nextLine());
+        double headsetPrice = readDoubleFromConsole(scanner);
+        double mousePrice = readDoubleFromConsole(scanner);
+        double keyboardPrice = readDoubleFromConsole(scanner);
+        double displayPrice = readDoubleFromConsole(scanner);
 
-        int lostHeadset = lostGamesCount / 2;
-        int lostMouse = lostGamesCount / 3;
-        int keyboardLost = lostGamesCount / 6;
-        int displayLost = 0;
+        int headsetTrashCount = calcTrashCount(lostGamesCount, 2);
+        int mouseTrashCount = calcTrashCount(lostGamesCount, 3);
+        int keyboardTrashCount = calcTrashCount(lostGamesCount, 6);
+        int displayTrashCount = calcDisplayTrashCount(keyboardTrashCount);
 
-        for (int i = 1; i <= keyboardLost; i++) {
+        double headsetCost = calcItemCost(headsetPrice, headsetTrashCount);
+        double mouseCost = calcItemCost(mousePrice, mouseTrashCount);
+        double keyboardCost = calcItemCost(keyboardPrice, keyboardTrashCount);
+        double displayCost = calcItemCost(displayPrice, displayTrashCount);
+        double totalCost = headsetCost + mouseCost + keyboardCost + displayCost;
+
+        printResult(totalCost);
+    }
+
+    private static double calcItemCost(double itemPrice, int trashesCount) {
+        return itemPrice * trashesCount;
+    }
+
+    private static int calcDisplayTrashCount(int keyboardTrashCount) {
+        int count = 0;
+
+        for (int i = 1; i <= keyboardTrashCount; i++) {
             if (i % 2 == 0) {
-                displayLost++;
+                count++;
             }
         }
+        return count;
+    }
 
-        double costs = headsetPrice * lostHeadset + mousePrice * lostMouse + keyboardPrice * keyboardLost + displayLost * displayPrice;
+    private static int calcTrashCount(int count, int divider) {
+        return count / divider;
+    }
 
-        System.out.printf("Rage expenses: %.2f lv.", costs);
+    private static void printResult(double totalCost) {
+        System.out.printf("Rage expenses: %.2f lv.", totalCost);
+    }
+
+    private static double readDoubleFromConsole(Scanner scanner) {
+        return Double.parseDouble(scanner.nextLine());
     }
 }
-
-
