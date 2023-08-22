1867d198febd16f68dfb673d108d5b1ab3ff01fa
wolfie <yavorhristozov@gmail.com>
2023-08-22 17:26:16 +0300

    refact : 03. Exact Sum of Real Numbers
    Split Main code by extracting simple method sumNumbers()

--- 02. Data Types and Variables/Lab/03. Exact Sum of Real Numbers/Main.java
+++ 02. Data Types and Variables/Lab/03. Exact Sum of Real Numbers/Main.java
@@ -6,14 +6,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = sumNumbers(n, scanner);
+
+        System.out.println(sum);
+    }
+
+    private static BigDecimal sumNumbers(int n, Scanner scanner) {
         BigDecimal sum = new BigDecimal(0);
 
         for (int i = 0; i < n; i++) {
-            BigDecimal next = new BigDecimal(scanner.nextLine());
-
-            sum = sum.add(next);
+            BigDecimal numberInput = new BigDecimal(scanner.nextLine());
+            sum = sum.add(numberInput);
         }
 
-        System.out.println(sum);
+        return sum;
     }
 }
