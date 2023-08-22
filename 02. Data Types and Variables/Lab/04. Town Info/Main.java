fd2fae73c14c764f3b60abf203b1d3a13d29c539
wolfie <yavorhristozov@gmail.com>
2023-08-22 17:30:30 +0300

    refact : 04. Town Info

--- 02. Data Types and Variables/Lab/04. Town Info/Main.java
+++ 02. Data Types and Variables/Lab/04. Town Info/Main.java
@@ -4,10 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String town = scanner.nextLine();
-        int population = Integer.parseInt(scanner.nextLine());
-        int area = Integer.parseInt(scanner.nextLine());
+        String city = scanner.nextLine();
+        int population = readIntegerInput(scanner);
+        int area = readIntegerInput(scanner);
 
-        System.out.printf(" Town %s has population of %d and area %d square km.", town, population, area);
+        printResult(city, population, area);
+    }
+
+    private static int readIntegerInput(Scanner scanner) {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void printResult(String city, int population, int area) {
+        System.out.printf("Town %s has population of %d and area %d square km.", city, population, area);
     }
 }
