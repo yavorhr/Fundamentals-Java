562916d670593dcf232002114f8b36b362a05466
wolfie <yavorhristozov@gmail.com>
2023-08-23 15:26:43 +0300

    refact : 09. Centuries to Minutes
    Split Main code by extracting method printCenturiesInMinutes()

--- 02. Data Types and Variables/Lab/09. Centuries to Minutes/Main.java
+++ 02. Data Types and Variables/Lab/09. Centuries to Minutes/Main.java
@@ -5,10 +5,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int centuries = Integer.parseInt(scanner.nextLine());
+        printCenturiesInMinutes(centuries);
+    }
 
+    private static void printCenturiesInMinutes(int centuries) {
         int years = centuries * 100;
         int days = (int) (years * 365.2422);
-        long hours = days * 24;
+        long hours = days * 24L;
         long minutes = hours * 60;
 
         System.out.printf("%d centuries = %d years = %d days = %d hours = %d minutes", centuries, years, days, hours, minutes);
