18019483b7e10e7c86e37d69ff60bb4c04a61bcc
wolfie <yavorhristozov@gmail.com>
2023-08-24 10:54:07 +0300

    refact : 02. Print Numbers in Reverse Order
    Split Main by extracting simple methods :
    initArray()
    printArrayReversed()

--- 03. Arrays/Lab/02. Print Numbers in Reverse Order/Main.java
+++ 03. Arrays/Lab/02. Print Numbers in Reverse Order/Main.java
@@ -4,16 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int count = Integer.parseInt(scanner.nextLine());
-        int[] numbers = new int[count];
+        int[] numbersArray = initArray(scanner);
+        printArrayReversed(numbersArray);
+    }
 
-        for (int i = 0; i < count; i++) {
-            numbers[i] = Integer.parseInt(scanner.nextLine());
+    private static int[] initArray(Scanner scanner) {
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int inputInt = Integer.parseInt(scanner.nextLine());
+            numbers[i] = inputInt;
         }
+        return numbers;
+    }
 
-        for (int i = count - 1; i >= 0; i--) {
-            System.out.print(numbers[i] + " ");
+    private static void printArrayReversed(int[] numbersArray) {
+        for (int i = numbersArray.length - 1; i >= 0; i--) {
+            System.out.print(numbersArray[i] + " ");
         }
     }
 }
-
