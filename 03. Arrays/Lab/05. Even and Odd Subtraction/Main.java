0ab4379aa4cff13abb088a0c666c7120b3e3b1da
wolfie <yavorhristozov@gmail.com>
2023-08-24 12:42:44 +0300

    refact : 05. Even and Odd Subtraction

--- 03. Arrays/Lab/05. Even and Odd Subtraction/Main.java
+++ 03. Arrays/Lab/05. Even and Odd Subtraction/Main.java
@@ -1,18 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] parts = scanner.nextLine().split(" ");
+        int[] inputNumbers = readIntArray(scanner);
 
-        for (int i = 0; i < parts.length / 2; i++) {
-            int swapIndex = parts.length - 1 - i;
-            String tmp = parts[i];
-            parts[i] = parts[swapIndex];
-            parts[swapIndex] = tmp;
-        }
+        int evenSum = Arrays.stream(inputNumbers).filter(e -> e % 2 == 0).sum();
+        int oddSum = Arrays.stream(inputNumbers).filter(e -> e % 2 != 0).sum();
 
-        System.out.println(String.join(" ", parts));
+        System.out.println(evenSum - oddSum);
+    }
+
+    private static int[] readIntArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
