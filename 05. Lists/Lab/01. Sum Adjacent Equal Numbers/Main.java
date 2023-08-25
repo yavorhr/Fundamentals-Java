ea08c89b02f3503e540881bc329478c2e2c1faa4
wolfie <yavorhristozov@gmail.com>
2023-08-25 18:56:36 +0300

    refact :  01. Sum Adjacent Equal Numbers
    Split Main by extracting simple methods :
    
    sumEqualNumbers()
    getNumberByIndex()
    readArrayList{}
    printResult{}

--- 05. Lists/Lab/01. Sum Adjacent Equal Numbers/Main.java
+++ 05. Lists/Lab/01. Sum Adjacent Equal Numbers/Main.java
@@ -1,39 +1,52 @@
-import java.util.Scanner;
-
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Double> numbers = new ArrayList<>();
-
-        String[] strings = scanner.nextLine().split(" ");
-
-        for (String string : strings) {
-            Double number = Double.parseDouble(string);
-            numbers.add(number);
-        }
+        List<Double> numbers = readArrayList(scanner);
 
         int index = 0;
         while (index < numbers.size() - 1) {
-            double currentElement = numbers.get(index);
-            double nextElement = numbers.get(index + 1);
+            double currentNumber = getNumberByIndex(numbers, index);
+            double nextNumber = getNumberByIndex(numbers, index + 1);
 
-            if (currentElement == nextElement) {
-                double sum = currentElement + nextElement;
-                numbers.set(index,sum);
-                numbers.remove(index + 1);
+            if (currentNumber == nextNumber) {
+                sumEqualNumbers(numbers, index, currentNumber, nextNumber);
                 index = 0;
             } else {
                 index++;
             }
         }
+        printResult(numbers);
+    }
 
-        for (double number : numbers) {
-            System.out.printf("%.0f ",number);
+    private static void sumEqualNumbers(List<Double> numbers, int index, double currentNumber, double nextNumber) {
+        double sum = currentNumber + nextNumber;
+        numbers.set(index, sum);
+        numbers.remove(index + 1);
+    }
+
+    private static double getNumberByIndex(List<Double> numbers, int index) {
+        return numbers.get(index);
+    }
+
+    private static List<Double> readArrayList(Scanner scanner) {
+        return
+                Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                        .map(Double::parseDouble)
+                        .collect(Collectors.toList());
+    }
+
+    private static void printResult(List<Double> arrayList) {
+        for (double number : arrayList) {
+            System.out.printf("%.0f ", number);
         }
     }
-}
 
+}
