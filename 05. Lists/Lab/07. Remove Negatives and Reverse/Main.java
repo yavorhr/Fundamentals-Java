b4af471e03fa0c11f0ac9652a4620ec089a812f8
wolfie <yavorhristozov@gmail.com>
2023-08-26 19:50:38 +0300

    refact : 07. Remove Negatives and Reverse

--- 05. Lists/Lab/07. Remove Negatives and Reverse/Main.java
+++ 05. Lists/Lab/07. Remove Negatives and Reverse/Main.java
@@ -1,31 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersAsText = scanner.nextLine().split(" ");
-        List<Integer> numbers = new ArrayList<>();
-        for (String num : numbersAsText) {
-            int number = Integer.parseInt(num);
-            if (number >= 0) {
-                numbers.add(number);
-            }
-        }
+        List<Integer> numbersList = readList(scanner);
 
-        Collections.reverse(numbers);
-        if (numbers.isEmpty()) {
-            System.out.println("empty");
-        } else {
-            System.out.println(numbers.toString().replaceAll("[\\[\\],]",""));
-        }
+        numbersList = filterListOfIntegers(numbersList);
+
+        Collections.reverse(numbersList);
+
+        printResult(numbersList);
+    }
+
+    private static List<Integer> readList(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> filterListOfIntegers(List<Integer> list) {
+        return list.stream().filter(e -> e > 0).collect(Collectors.toList());
+    }
+
+    private static void printResult(List<Integer> list) {
+        System.out.println(list.toString().replaceAll("[\\[\\],]", ""));
     }
 }
-
-
-
-
-
