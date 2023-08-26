5d509946937e9919faf3cfff46bee9126ce25304
wolfie <yavorhristozov@gmail.com>
2023-08-26 13:29:57 +0300

    refact :  02. Gauss' Trick
    Split Main code by extracting single methods

--- 05. Lists/Lab/02. Gauss' Trick/Main.java
+++ 05. Lists/Lab/02. Gauss' Trick/Main.java
@@ -1,64 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = new ArrayList<>();
-        String[] strings = scanner.nextLine().split(" ");
-        for (String string : strings) {
-            int number = Integer.parseInt(string);
-            numbers.add(number);
-        }
+        List<Integer> readListOfInts = readIntArrayList(scanner);
+        List<Integer> modifiedList = new ArrayList<>();
 
-        List<Integer> modify = new ArrayList<>();
+        sumFirstAndLastAndAddSumToNewArray(readListOfInts, modifiedList);
+        addLastElementToNewArrayIfLengthIsOdd(readListOfInts, modifiedList);
 
-        for (int i = 0; i < numbers.size() / 2; i++) {
-            int sum = numbers.get(i) + numbers.get(numbers.size()-1-i);
-            modify.add(sum);
-        }
+        printArrayList(modifiedList);
+    }
 
-        if (numbers.size() % 2 != 0) {
-            int middleIndex = numbers.size() / 2;
-            modify.add(numbers.get(middleIndex));
-        }
-
-        numbers = modify;
-
-        for (int number : modify) {
-            System.out.print(number + " ");
-
+    private static void addLastElementToNewArrayIfLengthIsOdd(List<Integer> readListOfInts, List<Integer> modifiedList) {
+        if (readListOfInts.size() % 2 != 0) {
+            int middleElementIndex = readListOfInts.size() / 2;
+            modifiedList.add(readListOfInts.get(middleElementIndex));
         }
     }
-}
 
-/*
-
-        String[] numbersInLine = scanner.nextLine().split(" ");
-        List<Integer> numbers = new ArrayList<>();
-
-        for (int i = 0; i < numbersInLine.length; i++) {
-            int currentNumber = Integer.parseInt(numbersInLine[i]);
-            numbers.add(currentNumber);
-        }
-
-        for (int i = 0; i < numbers.size(); i++) {
-            if (i == numbers.size() - 1) {
-                break;
-            }
-            int firstElement = numbers.get(i);
-            int lastElement = numbers.get(numbers.size() - 1);
-            int sum = firstElement + lastElement;
-            numbers.set(i, sum);
-            numbers.remove(numbers.size() - 1);
-        }
-
-        for (int i = 0; i < numbers.size(); i++) {
-            System.out.print(numbers.get(i) + " ");
+    private static void sumFirstAndLastAndAddSumToNewArray(List<Integer> readListOfInts, List<Integer> modifiedList) {
+        for (int i = 0; i < readListOfInts.size() / 2; i++) {
+            int sum = readListOfInts.get(i) + readListOfInts.get(readListOfInts.size() - 1 - i);
+            modifiedList.add(sum);
         }
     }
-}
 
- */
+    private static void printArrayList(List<Integer> modifiedList) {
+        System.out.println(modifiedList.toString().replaceAll("[\\[\\],]","" ));
+    }
+
+    private static List<Integer> readIntArrayList(Scanner scanner) {
+        List<Integer> readListOfInts =
+                Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+        return readListOfInts;
+    }
+}
