9438fced6d32a69d02e10e965c7396b3fe711cd9
wolfie <yavorhristozov@gmail.com>
2023-08-26 15:22:01 +0300

    refact : 04. List Manipulation Basics
    Split Main by extracting separate methods

--- 05. Lists/Lab/04. List Manipulation Basics/Main.java
+++ 05. Lists/Lab/04. List Manipulation Basics/Main.java
@@ -1,53 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersAsText = scanner.nextLine().split(" ");
-        List<Integer> numbers = new ArrayList<>();
-
-        for (String numberAsText : numbersAsText) {
-            int currentNum = Integer.parseInt(numberAsText);
-            numbers.add(currentNum);
-        }
+        List<Integer> listIntegers = readListIntegers(scanner);
 
         String input = scanner.nextLine();
-        while (!"end".equals(input)) {
-            String[] token = input.split(" ");
-            String command = token[0];
+        while (!input.equals("end")) {
+            String[] tokens = input.split(" ");
+            String command = tokens[0];
+
+            int number = Integer.parseInt(tokens[1]);
 
             switch (command) {
-                case "Add": {
-                    int addNumber = Integer.parseInt(token[1]);
-                    numbers.add(addNumber);
-                    break;
-                }
-                case "Remove": {
-                    int number = Integer.parseInt(token[1]);
-                    numbers.remove(Integer.valueOf(number));
-                    break;
-                }
-                case "RemoveAt": {
-                    int index = Integer.parseInt(token[1]);
-                    numbers.remove(index);
-                    break;
-                }
-                case "Insert": {
-                    int number = Integer.parseInt(token[1]);
-                    int index = Integer.parseInt(token[2]);
-                    numbers.add(index, number);
-                    break;
+                case "Add" -> addNumber(listIntegers, number);
+                case "Remove" -> removeNumber(listIntegers, number);
+                case "RemoveAt" -> removeAtIndex(listIntegers, number);
+                case "Insert" -> {
+                    int index = Integer.parseInt(tokens[2]);
+                    insertAtIndex(listIntegers, number, index);
                 }
             }
             input = scanner.nextLine();
         }
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
+        printModifiedList(listIntegers);
+    }
 
+    private static void insertAtIndex(List<Integer> listIntegers, int number, int index) {
+        listIntegers.add(index, number);
+    }
+
+    private static void removeAtIndex(List<Integer> listIntegers, int number) {
+        listIntegers.remove(number);
+    }
+
+    private static void removeNumber(List<Integer> listIntegers, int number) {
+        listIntegers.remove(Integer.valueOf(number));
+    }
+
+    private static void addNumber(List<Integer> listIntegers, int number) {
+        listIntegers.add(number);
+    }
+
+    private static void printModifiedList(List<Integer> listIntegers) {
+        System.out.println(listIntegers.toString().replaceAll("[\\[\\],]", ""));
+    }
+
+    private static List<Integer> readListIntegers(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
-
