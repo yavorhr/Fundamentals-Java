842c9a0510b360179090cd59ab05a602dd832e53
wolfie <yavorhristozov@gmail.com>
2023-08-26 19:39:43 +0300

    refact : 06. List of Products

--- 05. Lists/Lab/06. List of Products/Main.java
+++ 05. Lists/Lab/06. List of Products/Main.java
@@ -1,29 +1,27 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+        List<String> productsList = new ArrayList<>();
 
-        List<String> products = new ArrayList<>();
+        addProductsToList(scanner, n, productsList);
+        Collections.sort(productsList);
+        printResult(productsList);
+    }
 
-        for (int i = 0; i < n; i++) {
-            String currentProduct = scanner.nextLine();
-            products.add(currentProduct);
+    private static void printResult(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("%d.%s\n",i+1, list.get(i));
         }
+    }
 
-        Collections.sort(products);
-
-        for (int i = 0; i <products.size() ; i++) {
-            System.out.println (i+1  +"."+ products.get(i));
+    private static void addProductsToList(Scanner scanner, int n, List<String> list) {
+        for (int i = 0; i < n; i++) {
+            String product = scanner.nextLine();
+            list.add(product);
         }
     }
 }
-
-
-
-
