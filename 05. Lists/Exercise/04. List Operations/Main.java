52b11c1d605bb626d192e9fe0981eca26aa4f297
wolfie <yavorhristozov@gmail.com>
2023-08-27 13:48:35 +0300

    refact : . 04. List Operations

--- 05. Lists/Exercise/04. List Operations/Main.java
+++ 05. Lists/Exercise/04. List Operations/Main.java
@@ -1,70 +1,76 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersAsText = scanner.nextLine().split("\\s+");
-        List<Integer> numbers = new ArrayList<>();
-        for (String numberText : numbersAsText) {
-            int number = Integer.parseInt(numberText);
-            numbers.add(number);
-        }
+        List<Integer> integerList = readIntegersList(scanner);
 
         String input = scanner.nextLine();
-        while (!"End".equals(input)) {
-            String[] tokens = input.split("\\s+");
+        while (!input.equals("End")) {
+            String[] tokens = input.split(" ");
             String command = tokens[0];
-            switch (command) {
-                case "Add":
-                    int numberToAdd = Integer.parseInt(tokens[1]);
-                    numbers.add(numberToAdd);
-                    break;
-                case "Insert":
-                    int numberToInsert = Integer.parseInt(tokens[1]);
-                    int index = Integer.parseInt(tokens[2]);
-                    if (index >= 0 && index < numbers.size()) {
-                        numbers.add(index, numberToInsert);
-                    } else {
-                        System.out.println("Invalid index");
-                    }
-                    break;
-                case "Remove":
-                    int indexToRemove = Integer.parseInt(tokens[1]);
-                    if (indexToRemove >= 0 && indexToRemove < numbers.size()) {
-                        numbers.remove(indexToRemove);
-                    } else {
-                        System.out.println("Invalid index");
-                    }
-                    break;
-                case "Shift":
-                    String direction = tokens[1];
-                    int count = Integer.parseInt(tokens[2]);
-                    if (direction.equals("left")) {
-                        for (int i = 0; i < count; i++) {
-                            int temp = numbers.remove(0);
-                            numbers.add(temp);
-                        }
 
-                    } else if (direction.equals("right")) {
-                        for (int i = 0; i < count; i++) {
-                            int temp = numbers.remove(numbers.size()-1);
-                            numbers.add(0, temp);
-                        }
+            switch (command) {
+                case "Add" -> {
+                    int number = parseStringToInt(tokens[1]);
+                    integerList.add(number);
+                }
+                case "Remove" -> {
+                    int index = parseStringToInt(tokens[1]);
+                    if (outOfBounds(index, integerList)) {
+                        System.out.println("Invalid index");
+                        break;
                     }
-                    break;
+                    integerList.remove(index);
+                }
+                case "Shift" -> {
+                    String direction = tokens[1];
+                    int count = parseStringToInt(tokens[2]);
+                    shiftListToLeftOrRight(direction, integerList, count);
+                }
+                case "Insert" -> {
+                    int number = parseStringToInt(tokens[1]);
+                    int index = parseStringToInt(tokens[2]);
+                    integerList.add(index, number);
+                }
             }
             input = scanner.nextLine();
         }
-        System.out.println(numbers.toString().replaceAll("[\\[\\],]",""));
+
+        printResult(integerList);
+    }
+
+    private static boolean outOfBounds(int index, List<Integer> integerList) {
+        return index < 0 || index > integerList.size() - 1;
+    }
+
+    private static void shiftListToLeftOrRight(String direction, List<Integer> integerList, int count) {
+        if (direction.equals("left")) {
+            for (int i = 0; i < count; i++) {
+                int firstNumber = integerList.remove(0);
+                integerList.add(firstNumber);
+            }
+        } else if (direction.equals("right")) {
+            int lastNumber = integerList.remove(integerList.size() - 1);
+            integerList.add(0, lastNumber);
+        }
+    }
+
+    private static int parseStringToInt(String token) {
+        return Integer.parseInt(token);
+    }
+
+    private static List<Integer> readIntegersList(Scanner scanner) {
+        return
+                Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+    }
+
+    private static void printResult(List<Integer> integerList) {
+        System.out.println(integerList.toString().replaceAll("[\\[\\],]", ""));
     }
 }
-
-
-
-
-
-
-
