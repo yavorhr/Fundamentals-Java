923bc9911367f1e595357a20baab73e2880489db
wolfie <yavorhristozov@gmail.com>
2023-08-27 17:48:45 +0300

    refact : 05. Bomb Numbers

--- 05. Lists/Exercise/05. Bomb Numbers/Main.java
+++ 05. Lists/Exercise/05. Bomb Numbers/Main.java
@@ -1,41 +1,51 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersInLine = scanner.nextLine().split("\\s+");
+        List<Integer> numbers = readIntegersFromConsole(scanner);
+
         String[] commands = scanner.nextLine().split("\\s+");
-        int bomb = Integer.parseInt(commands[0]);
-        int power = Integer.parseInt(commands[1]);
-
-        List<Integer> numbers = new ArrayList<>();
-
-        for (String numberInLine : numbersInLine
-        ) {
-            int number = Integer.parseInt(numberInLine);
-            numbers.add(number);
-        }
+        int bomb = parseStringToInt(0, commands);
+        int power = parseStringToInt(1, commands);
 
         while (numbers.contains(bomb)) {
             int index = numbers.indexOf(bomb);
-            int leftBound = Math.max(index - power, 0);
-            int rightBound = Math.min(index + power, numbers.size() - 1);
-            for (int i = rightBound; i >= leftBound; i--) {
-                numbers.remove(i);
-            }
+            detonateBombAndRemoveAdjacentNumbers(numbers, power, index);
         }
+        printSum(numbers);
+    }
 
+    private static void printSum(List<Integer> numbers) {
         int result = 0;
-        for (int number : numbers
-        ) {
+        for (int number : numbers) {
             result += number;
         }
-
         System.out.println(result);
+    }
 
+    private static void detonateBombAndRemoveAdjacentNumbers(List<Integer> numbers, int power, int index) {
+        int leftBound = Math.max(index - power, 0);
+        int rightBound = Math.min(index + power, numbers.size() - 1);
+
+        for (int i = rightBound; i >= leftBound; i--) {
+            numbers.remove(i);
+        }
+    }
+
+    private static List<Integer> readIntegersFromConsole(Scanner scanner) {
+        return
+                Arrays.stream(scanner.nextLine()
+                        .split("\\s+"))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+    }
+
+    private static int parseStringToInt(int i, String[] commands) {
+        return Integer.parseInt(commands[i]);
     }
 }
+
 
