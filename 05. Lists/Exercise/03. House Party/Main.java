0ac4c2d0f8cbddd70a61dca4ffc3e84469f54fa4
wolfie <yavorhristozov@gmail.com>
2023-08-27 10:12:56 +0300

    refact : 03. House Party

--- 05. Lists/Exercise/03. House Party/Main.java
+++ 05. Lists/Exercise/03. House Party/Main.java
@@ -1,44 +1,48 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int commandsCount = Integer.parseInt(scanner.nextLine());
-        List<String> guests = new ArrayList<>();
+        List<String> partyList = new ArrayList<>();
 
-        for (int i = 0; i < commandsCount; i++) {
-            String[] tokens = scanner.nextLine().split("\\s+",2);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] tokens = scanner.nextLine().split(" ");
             String name = tokens[0];
-            String goingOrNot = tokens[1];
+            String goingOrNot = tokens[2];
 
-            switch (goingOrNot) {
-                case "is going!":
-                    if (guests.contains(name)) {
-                        System.out.printf("%s is already in the list!%n", name);
-                    } else {
-                        guests.add(name);
-                    }
-                    break;
-                case "is not going!":
-                    if (guests.contains(name)) {
-                        guests.remove(name);
-                    } else {
-                        System.out.printf("%s is not in the list!%n", name);
-                    }
-                    break;
+            modifyPartyList(name, goingOrNot, partyList);
+
+        }
+        printResult(partyList);
+    }
+
+    private static void modifyPartyList(String name, String goingOrNot, List<String> list) {
+        switch (goingOrNot) {
+            case "going!" -> {
+                boolean alreadyInvited = list.contains(name);
+
+                if (alreadyInvited) {
+                    System.out.printf("%s is already in the list!\n", name);
+                } else {
+                    list.add(name);
+                }
+            }
+            case "not" -> {
+                boolean cancelInvitation = list.contains(name);
+                if (cancelInvitation) {
+                    list.remove(name);
+                } else {
+                    System.out.printf("%s is not in the list!\n", name);
+                }
             }
         }
-        for (String guest : guests) {
-            System.out.println(guest);
+    }
+
+    private static void printResult(List<String> partyList) {
+        for (String name : partyList) {
+            System.out.println(name);
         }
     }
 }
-
-
-
-
-
-
