b71a0e68656502b848aa8803cfcead1e87b25d94
wolfie <yavorhristozov@gmail.com>
2023-08-28 15:02:00 +0300

    refact : 09. Pokemon Don't Go

--- 05. Lists/Exercise/09. Pokemon Don't Go/Main.java
+++ 05. Lists/Exercise/09. Pokemon Don't Go/Main.java
@@ -1,51 +1,50 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersInLine = scanner.nextLine().split(" ");
-        List<Integer> pokemonDistance = new ArrayList<>();
-
-        for (String number : numbersInLine) {
-            int currentPokemon = Integer.parseInt(number);
-            pokemonDistance.add(currentPokemon);
-        }
+        List<Integer> pokemonDistance =
+                Arrays.stream(scanner.nextLine().split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
 
         int valueOfRemovedElements = 0;
 
         while (!pokemonDistance.isEmpty()) {
             int index = Integer.parseInt(scanner.nextLine());
-            int indexToRemove = 0;
+            int removedElementValue = 0;
+            int lastIndex = pokemonDistance.size() - 1;
+
             if (index < 0) {
-                indexToRemove = pokemonDistance.remove(0);
-                valueOfRemovedElements += indexToRemove;
-                pokemonDistance.add(0, pokemonDistance.get(pokemonDistance.size() - 1));
-            } else if (index > pokemonDistance.size() - 1) {
-                indexToRemove = pokemonDistance.remove(pokemonDistance.size() - 1);
+                removedElementValue = getRemovedElementValue(pokemonDistance, 0);
+                pokemonDistance.add(0, pokemonDistance.get(lastIndex));
+                valueOfRemovedElements += removedElementValue;
+            } else if (index > lastIndex) {
+                removedElementValue = getRemovedElementValue(pokemonDistance, lastIndex);
                 pokemonDistance.add(pokemonDistance.get(0));
-                valueOfRemovedElements += indexToRemove;
+                valueOfRemovedElements += removedElementValue;
             } else {
-                indexToRemove = pokemonDistance.remove(index);
-                valueOfRemovedElements += indexToRemove;
+                removedElementValue = getRemovedElementValue(pokemonDistance, index);
+                valueOfRemovedElements += removedElementValue;
             }
 
             for (int i = 0; i < pokemonDistance.size(); i++) {
-                if (pokemonDistance.get(i) <= indexToRemove) {
-                    pokemonDistance.set(i, pokemonDistance.get(i) + indexToRemove);
+                if (pokemonDistance.get(i) <= removedElementValue) {
+                    pokemonDistance.set(i, pokemonDistance.get(i) + removedElementValue);
                 } else {
-                    pokemonDistance.set(i, pokemonDistance.get(i) - indexToRemove);
+                    pokemonDistance.set(i, pokemonDistance.get(i) - removedElementValue);
                 }
             }
         }
-
         System.out.println(valueOfRemovedElements);
     }
+
+    private static int getRemovedElementValue(List<Integer> pokemonDistance, int i2) {
+        int removedElementValue;
+        removedElementValue = pokemonDistance.remove(i2);
+        return removedElementValue;
+    }
 }
-
-
-
-
 
