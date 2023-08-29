7ea3679009b84f32d7395414dedfd246289360fc
wolfie <yavorhristozov@gmail.com>
2023-08-29 13:02:46 +0300

    refact : 03. Opinion Poll
    Reduce Main code by extracting single methods for better readability

--- 06. Objects and Classes/Exercise/03. Opinion Poll/src/Main.java
+++ 06. Objects and Classes/Exercise/03. Opinion Poll/src/Main.java
@@ -1,31 +1,33 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Person person;
+
         List<Person> personList = new ArrayList<>();
         int n = Integer.parseInt(scanner.nextLine());
 
+        addPersonsToList(scanner, personList, n);
+        printPersonsOver30SortedAlph(personList);
+    }
+
+    private static void printPersonsOver30SortedAlph(List<Person> personList) {
+        personList.stream()
+                .sorted((p1,p2) -> p1.getName().compareTo(p2.getName()))
+                .filter(p -> p.getAge() > 30)
+                .forEach(System.out::println);
+    }
+
+    private static void addPersonsToList(Scanner scanner, List<Person> personList, int n) {
         for (int i = 0; i < n; i++) {
-            String[] input = scanner.nextLine().split(" ");
-            person = new Person(input[0], Integer.parseInt(input[1]));
+            String[] tokens = scanner.nextLine().split(" ");
+            String name = tokens[0];
+            int age = Integer.parseInt(tokens[1]);
 
-            if (person.age > 30) {
-                personList.add(person);
-            }
+            Person person = new Person(name, age);
+            personList.add(person);
         }
-
-        Collections.sort(personList);
-        personList.forEach(p -> System.out.printf("%s - %d%n", p.name, p.age));
     }
 }
-
-
-
-
 
 
