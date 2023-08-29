5ab60ba9e7ddf6584ac704af9ea46b65d99d2b58
wolfie <yavorhristozov@gmail.com>
2023-08-29 12:24:10 +0300

    refact : 01. Advertisement Message

--- 06. Objects and Classes/Exercise/01. Advertisement Message/Main.java
+++ 06. Objects and Classes/Exercise/01. Advertisement Message/Main.java
@@ -1,40 +1,29 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] phrases = new String[]{"Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
-        String[] events = new String[]{"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
-        String[] authors = new String[]{"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
-        String[] cities = new String[]{"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+        String[] phrases = {"Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
+        String[] events = {"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
+        String[] authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+        String[] cities = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
 
-        int count = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+        printRandomPhrases(phrases, events, authors, cities, n);
+    }
 
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            String phrase = phrases[random.nextInt(phrases.length)];
-            String event = phrases[random.nextInt(events.length)];
-            String author = phrases[random.nextInt(authors.length)];
-            String city = phrases[random.nextInt(cities.length)];
+    private static void printRandomPhrases(String[] phrases, String[] events, String[] authors, String[] cities, int n) {
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++) {
+            String phrase = phrases[rnd.nextInt(phrases.length)];
+            String event = events[rnd.nextInt(events.length)];
+            String author = authors[rnd.nextInt(authors.length)];
+            String city = cities[rnd.nextInt(cities.length)];
 
-            System.out.printf("%s %s %s - %s%n", phrase, event, author, city);
+            System.out.printf("%s %s %s - %s\n", phrase, event, author, city);
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
