c600be8c051361338f2d29b9d1fac8c589c367ee
wolfie <yavorhristozov@gmail.com>
2023-08-29 20:48:58 +0300

    refact: 04. Articles 2.0

---
+++ 06. Objects and Classes/Exercise/04. Articles 2.0/src/Article.java
@@ -0,0 +1,43 @@
+public class Article {
+    private String title;
+    private String content;
+    private String author;
+
+    public Article(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Article setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Article setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Article setAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s:%s", this.title, this.content, this.author);
+    }
+}
