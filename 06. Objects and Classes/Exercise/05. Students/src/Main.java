3d501c78ae220c98faf6f8a34e8c3cfa23bf60c1
wolfie <yavorhristozov@gmail.com>
2023-08-29 21:12:55 +0300

    refact : 05. Students
    Refact Main

--- 06. Objects and Classes/Exercise/05. Students/src/Main.java
+++ 06. Objects and Classes/Exercise/05. Students/src/Main.java
@@ -1,49 +1,37 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-
-//Students
-//Write a program that receives n count of students and orders them by grade (in descending). Each student should
-//have first name (string), last name (string) and grade (floating-point number).
-//Input
-//• First line will be a number n
-//• Next n lines you will get a student info in the format "{first name} {second name} {grade}"
-//Output
-//• Print each student in the following format "{first name} {second name}: {grade}"
-//Example
-//Input
-//4
-//Lakia Eason 3.90
-//Prince Messing 5.49
-//Akiko Segers 4.85
-//Rocco Erben 6.00
-//Output:
-//Rocco Erben: 6.00
-//Prince Messing: 5.49
-//Akiko Segers: 4.85
-//Lakia Eason: 3.90
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = Integer.parseInt(scanner.nextLine());
         List<Student> students = new ArrayList<>();
 
-        while (n-- > 0) {
+        addStudentToList(scanner, n, students);
+        students = sortStudentsList(students);
+        printStudents(students);
+    }
+
+    private static void printStudents(List<Student> students) {
+        students.forEach(System.out::println);
+    }
+
+    private static List<Student> sortStudentsList(List<Student> students) {
+        return students.stream()
+                .sorted(Comparator.comparing(Student::getGrade).reversed())
+                .collect(Collectors.toList());
+    }
+
+    private static void addStudentToList(Scanner scanner, int n, List<Student> students) {
+        for (int i = 0; i < n; i++) {
             String[] input = scanner.nextLine().split(" ");
             String firstName = input[0];
-            String secondName = input[1];
+            String lastName = input[0];
             double grade = Double.parseDouble(input[2]);
-            Student student = new Student(firstName, secondName, grade);
+            Student student = new Student(firstName, lastName, grade);
             students.add(student);
         }
-
-        students.sort(Comparator.comparingDouble(Student::getGrade).reversed());
-        for (Student student : students) {
-            System.out.println(student.toString());
-        }
-
     }
 }
 
