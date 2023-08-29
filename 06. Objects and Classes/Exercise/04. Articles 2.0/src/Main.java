603681fb7179a209ae3de37064b774d1781bdfa1
wolfie <yavorhristozov@gmail.com>
2023-08-29 20:46:58 +0300

    refact: 04. Articles 2.0

--- 06. Objects and Classes/Exercise/04. Articles 2.0/src/Main.java
+++ 06. Objects and Classes/Exercise/04. Articles 2.0/src/Main.java
@@ -1,25 +1,56 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        List<Student> students = new ArrayList<>();
 
-        while (n-- > 0) {
-            String[] input = scanner.nextLine().split(" ");
-            String firstName = input[0];
-            String secondName = input[1];
-            double grade = Double.parseDouble(input[2]);
-            Student student = new Student(firstName, secondName, grade);
-            students.add(student);
+        List<Article> articleList = new ArrayList<>();
+        addArticlesToList(scanner, articleList);
+
+        String input = scanner.nextLine();
+        articleList = sortListByInput(articleList, input);
+
+        printResult(articleList);
+    }
+
+    private static void printResult(List<Article> articleList) {
+        articleList.forEach(System.out::println);
+    }
+
+    private static List<Article> sortListByInput(List<Article> list, String input) {
+        List<Article> sortedList = new ArrayList<>();
+
+        if (input.equals("title")) {
+            sortedList = list.stream().sorted(Comparator.comparing(Article::getTitle)).collect(Collectors.toList());
+        } else if (input.equals("author")) {
+            sortedList = list.stream().sorted(Comparator.comparing(Article::getAuthor)).collect(Collectors.toList());
+
+        } else if (input.equals("content")) {
+            sortedList = list.stream().sorted(Comparator.comparing(Article::getContent)).collect(Collectors.toList());
         }
-        students.sort(Comparator.comparingDouble(Student::getGrade).reversed());
-        for (Student student : students) {
-            System.out.println(student.toString());
+        return sortedList;
+    }
+
+    private static void addArticlesToList(Scanner scanner, List<Article> articleList) {
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            Article article = getArticle(input);
+            articleList.add(article);
         }
     }
+
+    private static Article getArticle(String[] input) {
+        String title = input[0];
+        String content = input[1];
+        String author = input[2];
+
+        return new Article(title, content, author);
+    }
+
+
 }
+
+
