8398ab381bf8d6e525015498b685432819e92820
wolfie <yavorhristozov@gmail.com>
2023-09-25 15:36:05 +0300

    refact : 01. Spring Vacation Trip

--- 11. Exams practice/Mid Exams/Mid Exam - 10 March 2019 Group 1/01. Spring Vacation Trip/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 10 March 2019 Group 1/01. Spring Vacation Trip/Main.java
@@ -6,76 +6,47 @@ public class Main {
 
         int days = Integer.parseInt(scanner.nextLine());
         double budget = Double.parseDouble(scanner.nextLine());
-        int peopleCnt = Integer.parseInt(scanner.nextLine());
-        double priceFuelPerKm = Double.parseDouble(scanner.nextLine());
-        double singleFoodExpenses = Double.parseDouble(scanner.nextLine());
-        double singleRoomExpensesPerNight = Double.parseDouble(scanner.nextLine());
+        int peopleCount = Integer.parseInt(scanner.nextLine());
+        double fuelPerKm = Double.parseDouble(scanner.nextLine());
+        double foodPerPerson = Double.parseDouble(scanner.nextLine());
+        double pricePerPersonForNight = Double.parseDouble(scanner.nextLine());
 
-        double nights = days * peopleCnt * singleRoomExpensesPerNight;
-        if (peopleCnt > 10) {
-            nights *= 0.75;
+        double totalFood = days * peopleCount * foodPerPerson;
+        double totalNights = days * peopleCount * pricePerPersonForNight;
+
+        if (peopleCount > 10) {
+            totalNights *= 0.75;
         }
-        double food = days * peopleCnt * singleFoodExpenses;
-        double expenses = food + nights;
-        boolean isSucessful = true;
+
+        double expenses = totalFood + totalNights;
+        boolean isSuccessful = true;
 
         for (int i = 1; i <= days; i++) {
-
-            double distanceKm = Double.parseDouble(scanner.nextLine());
-            expenses += distanceKm * priceFuelPerKm;
+            double travelledKm = Double.parseDouble(scanner.nextLine());
+            expenses += travelledKm * fuelPerKm;
 
             if (i % 3 == 0 || i % 5 == 0) {
                 expenses *= 1.4;
             }
             if (i % 7 == 0) {
-                expenses -= expenses / peopleCnt;
+                double reduceAmount = expenses / peopleCount;
+                expenses -= reduceAmount;
             }
+
             if (expenses > budget) {
-                isSucessful = false;
+                isSuccessful = false;
                 break;
             }
         }
 
-        if (isSucessful) {
-            System.out.printf("You have reached the destination. You have %.2f$ budget left.", budget - expenses);
+        if (isSuccessful) {
+            System.out.printf("You have reached the destination. You have %.2f$ budget left.", budget-expenses);
         } else {
-            System.out.printf("Not enough money to continue the trip. You need %.2f$ more.", expenses - budget);
+            System.out.printf("Not enough money to continue the trip. You need %.2f$ more.", expenses-budget);
         }
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
