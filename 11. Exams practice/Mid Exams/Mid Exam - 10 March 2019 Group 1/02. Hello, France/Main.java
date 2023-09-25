fb400ec754dc3c719040f9aa939355755398fd51
wolfie <yavorhristozov@gmail.com>
2023-09-25 18:06:16 +0300

    refact : 02. Hello, France

--- 11. Exams practice/Mid Exams/Mid Exam - 10 March 2019 Group 1/02. Hello, France/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 10 March 2019 Group 1/02. Hello, France/Main.java
@@ -1,99 +1,91 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] items = scanner.nextLine().split("\\|");
+        String[] input = scanner.nextLine().split("\\|");
         double budget = Double.parseDouble(scanner.nextLine());
-        List<Double> soldItems = new ArrayList<>();
+        List<Double> boughtItems = new ArrayList<>();
 
-        for (int i = 0; i < items.length; i++) {
-            String[] item = items[i].split("->");
-            String name = item[0];
-            double price = Double.parseDouble(item[1]);
+        for (String string : input) {
+            String[] tokens = string.split("->");
+            String type = tokens[0];
+            double price = Double.parseDouble(tokens[1]);
 
-            switch (name) {
-                case "Clothes":
-                    if (price <= 50) {
-                        if (budget - price >= 0) {
-                            budget -= price;
-                            soldItems.add(price);
-                        }
+            switch (type) {
+                case "Clothes" -> {
+                    boolean enoughBudget = checkIfBudgetIsEnough(price, budget);
+                    if (enoughBudget && priceIsInRange(price, 50)) {
+                        budget = addItem(budget, boughtItems, price);
                     }
-                    break;
-                case "Shoes":
-                    if (price <= 35.50) {
-                        if (budget - price >= 0) {
-                            budget -= price;
-                            soldItems.add(price);
-                        }
+                }
+                case "Shoes" -> {
+                    boolean enoughBudget = checkIfBudgetIsEnough(price, budget);
+                    if (enoughBudget && priceIsInRange(price, 35)) {
+                        budget = addItem(budget, boughtItems, price);
                     }
-                    break;
-                case "Accessories":
-                    if (price <= 25.50) {
-                        if (budget - price >= 0) {
-                            budget -= price;
-                            soldItems.add(price);
-                        }
+                }
+                case "Accessories" -> {
+                    boolean enoughBudget = checkIfBudgetIsEnough(price, budget);
+                    if (enoughBudget && priceIsInRange(price, 20.5)) {
+                        budget = addItem(budget, boughtItems, price);
                     }
-                    break;
+                }
             }
         }
-        double allProfit = 0;
-        double sum = 0;
-        for (int i = 0; i < soldItems.size(); i++) {
-            double profit = soldItems.get(i) * 0.4;
-            allProfit += profit;
-            soldItems.set(i, profit + soldItems.get(i));
 
-        }
+        List<Double> soldItems = addProfit(boughtItems);
 
-        for (double price : soldItems) {
-            System.out.printf("%.2f ", price);
-            sum += price;
-        }
-        System.out.println();
-        System.out.printf("Profit: %.2f%n", allProfit);
-        if (budget + sum >= 150) {
-            System.out.println("Hello, France!");
+        double sumOfBoughtItems = getSum(boughtItems);
+        double sumOfSoldItems = getSum(soldItems);
+        double newBudget = budget + sumOfSoldItems;
+
+        if (newBudget >= 150) {
+            printResult(sumOfSoldItems, sumOfBoughtItems, "Hello, France!", soldItems);
         } else {
-            System.out.println("Time to go.");
+            printResult(sumOfSoldItems, sumOfBoughtItems, "Time to go.", soldItems);
         }
     }
+
+    private static List<Double> addProfit(List<Double> boughtItems) {
+        return
+                boughtItems
+                        .stream()
+                        .map(item -> item *= 1.4)
+                        .collect(Collectors.toList());
+    }
+
+    private static double getSum(List<Double> boughtItems) {
+        return
+                boughtItems
+                        .stream()
+                        .mapToDouble(Double::doubleValue)
+                        .sum();
+    }
+
+    private static void printResult(double sumSoldItems, double sumBoughtItems, String result, List<Double> soldItems) {
+        soldItems.forEach(item -> System.out.printf("%.2f ", item));
+        System.out.println();
+        System.out.printf("Profit: %.2f\n", sumSoldItems - sumBoughtItems);
+        System.out.println(result);
+    }
+
+    private static double addItem(double budget, List<Double> soldItems, double price) {
+        budget -= price;
+        soldItems.add(price);
+        return budget;
+    }
+
+    private static boolean priceIsInRange(double currentPrice, double bound) {
+        return currentPrice <= bound;
+    }
+
+    private static boolean checkIfBudgetIsEnough(double price, double budget) {
+        return budget - price >= 0;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
