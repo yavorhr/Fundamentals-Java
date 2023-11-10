044b00816f349c5f6bf9509f291cfae23d640f72
wolfie <yavorhristozov@gmail.com>
2023-11-10 15:18:45 +0200

    refact : 01. Hunting Games

--- 11. Exams practice/Mid Exams/Mid Exam - 10 March 2019 Group 2/01. The Hunting Games/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 10 March 2019 Group 2/01. The Hunting Games/Main.java
@@ -4,74 +4,54 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int daysAdventure = Integer.parseInt(scanner.nextLine());
-        int playersCnt = Integer.parseInt(scanner.nextLine());
-        double energyGroup = Double.parseDouble(scanner.nextLine());
-        double waterPerDay = Double.parseDouble(scanner.nextLine());
-        double foodPerDay = Double.parseDouble(scanner.nextLine());
+        int days = Integer.parseInt(scanner.nextLine());
+        int countOfPeople = Integer.parseInt(scanner.nextLine());
+        double groupEnergy = Double.parseDouble(scanner.nextLine());
 
-        double waterGroup = waterPerDay * playersCnt * daysAdventure;
-        double foodGroup = foodPerDay * playersCnt * daysAdventure;
+        double waterPerPlayer = Double.parseDouble(scanner.nextLine());
+        double foodPerPlayer = Double.parseDouble(scanner.nextLine());
+
+        double currentWater = getCurrentSupply(days, countOfPeople, waterPerPlayer);
+        double currentFood = getCurrentSupply(days, countOfPeople, foodPerPlayer);
+
         boolean isSuccessful = true;
 
-        for (int i = 1; i <= daysAdventure; i++) {
-            double energyLoss = Double.parseDouble(scanner.nextLine());
-            if (energyGroup - energyLoss > 0) {
-                energyGroup -= energyLoss;
-            } else {
+        for (int i = 1; i <= days; i++) {
+            double energyLossPerDay = Double.parseDouble(scanner.nextLine());
+
+            if (groupEnergy - energyLossPerDay <= 0) {
                 isSuccessful = false;
                 break;
             }
 
+            groupEnergy -= energyLossPerDay;
+
             if (i % 2 == 0) {
-                energyGroup *= 1.05;
-                waterGroup = waterGroup - waterGroup * 0.3;
+                groupEnergy *= 1.05;
+                currentWater *= 0.7;
             }
 
             if (i % 3 == 0) {
-                energyGroup *= 1.1;
-                foodGroup = foodGroup - (foodGroup / playersCnt);
+                groupEnergy *= 1.1;
+                currentFood -= currentFood / countOfPeople;
             }
         }
 
-        if (isSuccessful) {
-            System.out.printf("You are ready for the quest. You will be left with - %.2f energy!", energyGroup);
-        } else {
-            System.out.printf("You will run out of energy. You will be left with %.2f food and %.2f water.", foodGroup, waterGroup);
-        }
+        printResult(groupEnergy, currentWater, currentFood, isSuccessful);
+    }
 
+    private static double getCurrentSupply(int days, int countOfPeople, double waterPerPlayer) {
+        return waterPerPlayer * countOfPeople * days;
+    }
+
+    private static void printResult(double groupEnergy, double currentWater, double currentFood, boolean isSuccessful) {
+        if (isSuccessful) {
+            System.out.printf("You are ready for the quest. You will be left with - %.2f energy!", groupEnergy);
+        } else {
+            System.out.printf("You will run out of energy. You will be left with %.2f food and %.2f water.", currentFood, currentWater);
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
