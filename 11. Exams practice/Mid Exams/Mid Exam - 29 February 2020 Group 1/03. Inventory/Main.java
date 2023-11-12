6b6c8af6507c35edede3c1f24c6442d75adc9101
wolfie <yavorhristozov@gmail.com>
2023-11-12 16:44:27 +0200

    refact : 01. Inventory
    Add methods
    Change variables names for better readability

--- 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 1/03. Inventory/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 1/03. Inventory/Main.java
@@ -1,62 +1,61 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] itemsText = scanner.nextLine().split(", ");
-        List<String> items = new ArrayList<>();
-        items.addAll(Arrays.asList(itemsText));
+        List<String> items =
+                Arrays.stream(scanner.nextLine()
+                        .split(", "))
+                        .collect(Collectors.toList());
 
         String input = scanner.nextLine();
         while (!"Craft!".equals(input)) {
             String[] tokens = input.split(" - ");
             String command = tokens[0];
-            String currentItem = tokens[1];
+            String currentITem = tokens[1];
 
             switch (command) {
-                case "Collect":
-                    if (!items.contains(currentItem)) {
-                        items.add(currentItem);
+                case "Collect" -> {
+                    if (!isItemExisting(items, currentITem)) {
+                        items.add(currentITem);
                     }
-                    break;
-                case "Drop":
-                    if (items.contains(currentItem)) {
-                        items.remove(currentItem);
-                    }
-                    break;
-                case "Combine Items":
-                    String[] newSplit = currentItem.split(":");
-                    String oldItem = newSplit[0];
-                    String newItem = newSplit[1];
+                }
+                case "Drop" -> {
+                    items.remove(currentITem);
+                }
+                case "Combine Items" -> {
+                    String[] inputItems = currentITem.split(": ");
+                    String oldItem = inputItems[0];
+                    String newItem = inputItems[1];
 
-                    if (items.contains(oldItem)) {
-                        int index = items.indexOf(oldItem);
-                        items.add(index + 1, newItem);
+                    if (isItemExisting(items, oldItem)) {
+                        int oldItemIndex = items.indexOf(oldItem);
+                        items.add(oldItemIndex + 1, newItem);
                     }
-                    break;
-                case "Renew":
-                    if (items.contains(currentItem)) {
-                        items.remove(currentItem);
-                        items.add(currentItem);
+                }
+                case "Renew" -> {
+                    if (isItemExisting(items, currentITem)) {
+                        items.remove(currentITem);
+                        items.add(currentITem);
                     }
-                    break;
+                }
             }
             input = scanner.nextLine();
         }
+        printItems(items);
+    }
+
+    private static void printItems(List<String> items) {
         System.out.println(String.join(", ", items));
     }
+
+    private static boolean isItemExisting(List<String> items, String currentItem) {
+        return items.contains(currentItem);
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
