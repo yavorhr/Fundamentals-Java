68190a9e13361fe39ac3576b8ee1d4a322cec185
wolfie <yavorhristozov@gmail.com>
2023-11-12 15:11:00 +0200

    refact : 01. Bonus Scoring System
    Add methods and change variable names for better readability of the code

--- 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 1/01. Bonus Scoring System/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 1/01. Bonus Scoring System/Main.java
@@ -4,37 +4,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int studentsCnt = Integer.parseInt(scanner.nextLine());
-        int lecturesCnt = Integer.parseInt(scanner.nextLine());
+        int studentsCount = Integer.parseInt(scanner.nextLine());
+        int lecturesCount = Integer.parseInt(scanner.nextLine());
         int bonus = Integer.parseInt(scanner.nextLine());
 
-
         double maxBonus = 0;
-        int bestAttend = 0;
+        int mostAttended = 0;
 
-        for (int i = 0; i < studentsCnt; i++) {
-            int attendanciesCnt = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < studentsCount; i++) {
+            int currentAttendance = Integer.parseInt(scanner.nextLine());
 
-            if (attendanciesCnt > bestAttend) {
-                bestAttend = attendanciesCnt;
-            }
-            double currentBonus = (attendanciesCnt * 1.0 / (lecturesCnt * 1.0)) * (5 + bonus);
+            double currentBonus = (currentAttendance * 1.0 / (lecturesCount * 1.0)) * (5 + bonus);
+
             if (currentBonus > maxBonus) {
                 maxBonus = currentBonus;
-
+                mostAttended = currentAttendance;
             }
         }
+
+        printResult(maxBonus, mostAttended);
+    }
+
+    private static void printResult(double maxBonus, int mostAttended) {
         System.out.printf("Max Bonus: %.0f.%n", maxBonus);
-        System.out.printf("The student has attended %d lectures.%n", bestAttend);
+        System.out.printf("The student has attended %d lectures.%n", mostAttended);
     }
 }
-
-
-
-
-
-
-
 
 
 
