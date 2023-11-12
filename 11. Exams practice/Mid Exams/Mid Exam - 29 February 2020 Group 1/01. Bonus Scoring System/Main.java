31c9740ade616b8812fc39039c0ca8400692acfa
wolfie <yavorhristozov@gmail.com>
2023-11-12 16:07:24 +0200

    refact : 02. Mu Online
    Add methods
    Change variable names for better readability

--- 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 1/01. Bonus Scoring System/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 1/01. Bonus Scoring System/Main.java
@@ -4,33 +4,63 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int studentsCount = Integer.parseInt(scanner.nextLine());
-        int lecturesCount = Integer.parseInt(scanner.nextLine());
-        int bonus = Integer.parseInt(scanner.nextLine());
+        String[] input = scanner.nextLine().split("\\|");
+        int health = 100;
+        int bitcoins = 0;
+        boolean isSuccessful = true;
 
-        double maxBonus = 0;
-        int mostAttended = 0;
 
-        for (int i = 0; i < studentsCount; i++) {
-            int currentAttendance = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < input.length; i++) {
+            String[] tokens = input[i].split(" ");
+            String command = tokens[0];
+            int value = Integer.parseInt(tokens[1]);
 
-            double currentBonus = (currentAttendance * 1.0 / (lecturesCount * 1.0)) * (5 + bonus);
-
-            if (currentBonus > maxBonus) {
-                maxBonus = currentBonus;
-                mostAttended = currentAttendance;
+            switch (command) {
+                case "potion" -> {
+                    int diff = 100 - health;
+                    if (health + value > 100) {
+                        printOutput(diff, "You healed for %d hp.\n");
+                        health = 100;
+                    } else {
+                        printOutput(value, "You healed for %d hp.\n");
+                        health += value;
+                    }
+                    printOutput(health, "Current health: %d hp.\n");
+                }
+                case "chest" -> {
+                    bitcoins += value;
+                    printOutput(value, "You found %d bitcoins.\n");
+                }
+                default -> {
+                    if (health - value > 0) {
+                        health -= value;
+                        System.out.printf("You slayed %s.\n", command);
+                    } else {
+                        System.out.printf("You died! Killed by %s.\n", command);
+                        printOutput(i + 1, "Best room: %d\n");
+                        isSuccessful = false;
+                    }
+                }
+            }
+            if (!isSuccessful) {
+                break;
             }
         }
-
-        printResult(maxBonus, mostAttended);
+        printIfIsSuccessful(health, bitcoins, isSuccessful);
     }
 
-    private static void printResult(double maxBonus, int mostAttended) {
-        System.out.printf("Max Bonus: %.0f.%n", maxBonus);
-        System.out.printf("The student has attended %d lectures.%n", mostAttended);
+    private static void printOutput(int diff, String s) {
+        System.out.printf(s, diff);
+    }
+
+    private static void printIfIsSuccessful(int health, int bitcoins, boolean isSuccessful) {
+        if (isSuccessful) {
+            System.out.println("You've made it!");
+            System.out.println("Bitcoins: " + bitcoins);
+            System.out.println("Health: " + health);
+        }
     }
 }
-
 
 
 
