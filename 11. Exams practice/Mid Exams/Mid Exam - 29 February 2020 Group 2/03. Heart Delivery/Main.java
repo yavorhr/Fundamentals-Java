7a4dba0079d945031ffe44214ae4403d7e9e1c1e
wolfie <yavorhristozov@gmail.com>
2023-11-14 12:38:49 +0200

    refact : 03. Heart Delivery
    add methods

--- 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 2/03. Heart Delivery/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 29 February 2020 Group 2/03. Heart Delivery/Main.java
@@ -31,15 +31,21 @@ public class Main {
             input = scanner.nextLine();
         }
 
-        int sum = Arrays.stream(houses).sum();
-
-        if (doesElementEqualToZero(sum)) {
+        if (allElementsEqualToZero(houses)) {
             System.out.println("Mission was successful.");
         } else {
-            long count = Arrays.stream(houses).filter(h -> h != 0).count();
-            System.out.printf("Cupid's last position was %d.\n", currentIndex);
-            System.out.printf("Cupid has failed %d places.\n", count);
+            printResult(houses,currentIndex);
         }
+    }
+
+    private static void printResult(int[] houses, int index) {
+        long count = Arrays.stream(houses).filter(h -> h != 0).count();
+        System.out.printf("Cupid's last position was %d.\n", index);
+        System.out.printf("Cupid has failed %d places.\n", count);
+    }
+
+    private static boolean allElementsEqualToZero(int[] houses) {
+        return Arrays.stream(houses).sum() == 0;
     }
 
     private static boolean doesElementEqualToZero(int house) {
