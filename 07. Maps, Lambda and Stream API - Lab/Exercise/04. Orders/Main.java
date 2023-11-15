180d2e522acbdd8b243ee3c4ca1d7e5672572afb
wolfie <yavorhristozov@gmail.com>
2023-11-15 19:23:17 +0200

    refact : 04. Orders
    change variable names
    add print method

--- 07. Maps, Lambda and Stream API - Lab/Exercise/04. Orders/Main.java
+++ 07. Maps, Lambda and Stream API - Lab/Exercise/04. Orders/Main.java
@@ -4,25 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> quantityMap = new LinkedHashMap<>();
-        Map<String, Double> priceMap = new LinkedHashMap<>();
+        HashMap<String, Double> priceMap = new HashMap<>();
+        LinkedHashMap<String, Integer> quantityMap = new LinkedHashMap<>();
 
-        String line = scanner.nextLine();
-        while (!"buy".equals(line)) {
-            String[] products = line.split("\\s+");
-            String productKey = products[0];
-            double price = Double.parseDouble(products[1]);
-            int quantity = Integer.parseInt(products[2]);
+        String input = scanner.nextLine();
+        while (!"buy".equals(input)) {
+            String[] tokens = input.split(" ");
+            String productKey = tokens[0];
+
+            double price = Double.parseDouble(tokens[1]);
+            int quantity = Integer.parseInt(tokens[2]);
 
             priceMap.put(productKey, price);
             quantityMap.putIfAbsent(productKey, 0);
-            int newQuantity = quantityMap.get(productKey) + quantity;
-            quantityMap.put(productKey, newQuantity);
 
-            line = scanner.nextLine();
+            int newQuantity = quantityMap.get(productKey) + quantity;
+            quantityMap.put(productKey,newQuantity);
+
+            input = scanner.nextLine();
         }
 
-        quantityMap.forEach((k, v) -> System.out.println(String.format("%s -> %.2f", k, v * priceMap.get(k))));
+        quantityMap.forEach((k, v) -> System.out.printf("%s -> %.2f\n", k, v * priceMap.get(k)));
     }
 }
+
+
 
