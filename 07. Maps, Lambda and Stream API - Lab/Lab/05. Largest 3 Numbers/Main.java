a5ac3bac6ea5d84c9226506de393fca30737884f
wolfie <yavorhristozov@gmail.com>
2023-11-15 15:05:18 +0200

    refact : 05. Largest 3 Numbers

--- 07. Maps, Lambda and Stream API - Lab/Lab/05. Largest 3 Numbers/Main.java
+++ 07. Maps, Lambda and Stream API - Lab/Lab/05. Largest 3 Numbers/Main.java
@@ -1,18 +1,17 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> nums = Arrays
-                .stream(scanner.nextLine().split(" "))
-                .map(e -> Integer.parseInt(e))
+        Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .map(Integer::parseInt)
                 .sorted((n1, n2) -> n2.compareTo(n1))
                 .limit(3)
-                .collect(Collectors.toList());
-        for (int num : nums) {
-            System.out.print(num + " ");
-        }
+                .forEach(n -> System.out.printf("%d ", n));
+
     }
 }
+
+
