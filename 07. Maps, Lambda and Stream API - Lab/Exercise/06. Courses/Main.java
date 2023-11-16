32151c629bbf8363fed58b16a9be65fa19cd3062
wolfie <yavorhristozov@gmail.com>
2023-11-16 11:44:45 +0200

    refact : 06. Courses
    Add methods to split main
    change variables names

--- 07. Maps, Lambda and Stream API - Lab/Exercise/06. Courses/Main.java
+++ 07. Maps, Lambda and Stream API - Lab/Exercise/06. Courses/Main.java
@@ -7,15 +7,39 @@ public class Main {
         HashMap<String, List<String>> courses = new HashMap<>();
 
         String input = scanner.nextLine();
-        while ("!end".equals(input)) {
+        while (!"end".equals(input)) {
             String[] tokens = input.split(" : ");
             String courseName = tokens[0];
             String studentName = tokens[1];
 
+            courses.putIfAbsent(courseName, new ArrayList<>());
 
+            addStudentToCourse(courses, courseName, studentName);
 
             input = scanner.nextLine();
         }
+        printResult(courses);
+    }
+
+    private static void printResult(HashMap<String, List<String>> courses) {
+        courses
+                .entrySet()
+                .stream()
+                .sorted((courses1, course2) -> Integer.compare(course2.getValue().size(), courses1.getValue().size()))
+                .forEach(entry -> {
+                    System.out.printf("%s: %d\n", entry.getKey(), entry.getValue().size());
+
+                    entry.getValue()
+                            .stream()
+                            .sorted((student1, student2) -> student1.compareTo(student2))
+                            .forEach(s -> System.out.printf("-- %s\n", s));
+                });
+    }
+
+    private static void addStudentToCourse(HashMap<String, List<String>> courses, String courseName, String studentName) {
+        List<String> students = courses.get(courseName);
+        students.add(studentName);
+        courses.put(courseName, students);
     }
 }
 
