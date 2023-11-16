58865a5e13a9c636671b211238fcc295f6dee212
wolfie <yavorhristozov@gmail.com>
2023-11-16 14:00:46 +0200

    refact 01. Distance Calculator
    remove for loop
    Add simple arithmetic operations to calculate percent of covered area

--- 11. Exams practice/Mid Exams/Mid Exam - 30 June 2019 Group 1/01. Distance Calculator/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 30 June 2019 Group 1/01. Distance Calculator/Main.java
@@ -5,50 +5,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int steps = Integer.parseInt(scanner.nextLine());
-        double lengthStep = Double.parseDouble(scanner.nextLine());
-        int distanceTarget = Integer.parseInt(scanner.nextLine());
-        double sumSteps = 0;
+        double lengthPerStepInCm = Double.parseDouble(scanner.nextLine());
+        int distanceInMeters = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= steps; i++) {
-            double currentStep = lengthStep;
-            if (i % 5 == 0) {
-                currentStep = lengthStep * 0.7;
-            }
-            sumSteps += currentStep;
-        }
+        int shorterStepsCount = steps / 5;
+        double shorterStepsCoverageInCm = shorterStepsCount * (lengthPerStepInCm * 0.7);
 
-        sumSteps /=100;
-        double percentage = (sumSteps/distanceTarget)*100;
+        double coverDistanceInCm = ((steps - shorterStepsCount) * lengthPerStepInCm) + shorterStepsCoverageInCm;
+        double coveredDistanceInMeters = coverDistanceInCm / 100;
 
-        System.out.printf("You travelled %.2f%% of the distance!",percentage);
+        double coveredPercent = coveredDistanceInMeters / distanceInMeters * 100;
+
+        System.out.printf("You travelled %.2f%% of the distance!", coveredPercent);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
