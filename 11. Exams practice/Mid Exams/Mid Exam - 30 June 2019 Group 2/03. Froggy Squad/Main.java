c2d570fa68cb780c017a102c19adaa204f8258bd
wolfie <yavorhristozov@gmail.com>
2023-11-19 17:16:11 +0200

    refact : 03. Froggy Squad
    Add methods
    Rename variables

--- 11. Exams practice/Mid Exams/Mid Exam - 30 June 2019 Group 2/03. Froggy Squad/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam - 30 June 2019 Group 2/03. Froggy Squad/Main.java
@@ -10,14 +10,77 @@ public class Main {
                 .collect(Collectors.toList());
 
         String input = scanner.nextLine();
-
         while (true) {
             String[] tokens = input.split(" ");
-            if (input.contains("Print")) {
+            String command = tokens[0];
+            String condition = tokens[1];
 
+            if (command.equals("Print")) {
+                switch (condition) {
+                    case "Normal" -> {
+                        printCollection(frogs);
+                    }
+                    case "Reversed" -> {
+                        Collections.reverse(frogs);
+                        printCollection(frogs);
+                    }
+                }
+                break;
             }
 
+            switch (command) {
+                case "Join" -> {
+                    String name = tokens[1];
+                    frogs.add(name);
+                }
+                case "Dive" -> {
+                    int index = Integer.parseInt(tokens[1]);
+                    if (isIndexValid(frogs, index)) {
+                        frogs.remove(index);
+                    }
+                }
+                case "Jump" -> {
+                    String name = tokens[1];
+                    int index = Integer.parseInt(tokens[2]);
+                    if (isIndexValid(frogs, index)) {
+                        frogs.add(index, name);
+                    }
+                }
+                case "First" -> {
+                    int count = Integer.parseInt(tokens[1]);
+                    int rightBound = Math.min(count, frogs.size());
+
+                    printCollectionByIndexes(0, rightBound, frogs);
+                }
+                case "Last" -> {
+                    int count = Integer.parseInt(tokens[1]);
+                    int startIndex = frogs.size() - count;
+
+                    if (startIndex < 0) {
+                        System.out.println(String.join(" ", frogs));
+                    } else {
+                        printCollectionByIndexes(startIndex, frogs.size(), frogs);
+                    }
+                }
+            }
+            input = scanner.nextLine();
         }
+    }
+
+    private static boolean isIndexValid(List<String> frogs, int index) {
+        return index >= 0 && index < frogs.size();
+    }
+
+    private static void printCollection(List<String> frogs) {
+        System.out.print("Frogs: ");
+        System.out.print(String.join(" ", frogs));
+    }
+
+    private static void printCollectionByIndexes(int startIndex, int endIndex, List<String> frogs) {
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.print(frogs.get(i) + " ");
+        }
+        System.out.println();
     }
 }
 
