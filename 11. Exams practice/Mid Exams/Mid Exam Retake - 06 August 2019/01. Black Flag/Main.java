add2e129cb2577c2a07224d42c5396e1b6709efb
wolfie <yavorhristozov@gmail.com>
2023-11-20 13:15:56 +0200

    refact : 01. Black Flag
    Add Methods
    Rename variables

--- 11. Exams practice/Mid Exams/Mid Exam Retake - 06 August 2019/01. Black Flag/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam Retake - 06 August 2019/01. Black Flag/Main.java
@@ -1,50 +1,48 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int days = Integer.parseInt(scanner.nextLine());
-        int dailyPlunder = Integer.parseInt(scanner.nextLine());
+        int plunderPerDay = Integer.parseInt(scanner.nextLine());
         double expectedPlunder = Double.parseDouble(scanner.nextLine());
-        double totalPlunder = 0;
 
-        for (int i = 1; i <= days; i++) {
-            double currentPlunder = dailyPlunder * 1.0;
-            if (i % 3 == 0) {
-                currentPlunder = currentPlunder + currentPlunder * 0.5;
-            }
+        double collectedPlunder = 0.0;
 
-            totalPlunder += currentPlunder;
+        collectedPlunder = addPlunder(days, plunderPerDay, collectedPlunder);
 
-            if (i % 5 == 0) {
-                totalPlunder *= 0.7;
-            }
-        }
-        if (totalPlunder >= expectedPlunder) {
-            System.out.printf("Ahoy! %.2f plunder gained.%n", totalPlunder);
+        printResult(expectedPlunder, collectedPlunder);
+
+    }
+
+    private static void printResult(double expectedPlunder, double collectedPlunder) {
+        if (collectedPlunder >= expectedPlunder) {
+            System.out.printf("Ahoy! %.2f plunder gained.", collectedPlunder);
         } else {
-            double percentage = (totalPlunder / expectedPlunder) * 100;
-            System.out.printf("Collected only %.2f%% of the plunder.", percentage);
+            double percents = (collectedPlunder / expectedPlunder) * 100;
+            System.out.printf("Collected only %.2f%% of the plunder.", percents);
         }
     }
+
+    private static double addPlunder(int days, int plunderPerDay, double collectedPlunder) {
+        for (int i = 1; i <= days; i++) {
+            double currentPlunder = plunderPerDay;
+
+            if (i % 3 == 0) {
+                currentPlunder *= 1.5;
+            }
+
+            collectedPlunder += currentPlunder;
+
+            if (i % 5 == 0) {
+                collectedPlunder *= 0.7;
+            }
+        }
+        return collectedPlunder;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
