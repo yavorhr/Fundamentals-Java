c293090f5e8be752984e75d5c783bcfd2c5e8429
wolfie <yavorhristozov@gmail.com>
2023-11-21 16:26:33 +0200

    refact : 01. Reverse Strings
    Rename variables
    Add methods to split Main

--- 08. Text Processing/Lab/01. Reverse Strings/Main.java
+++ 08. Text Processing/Lab/01. Reverse Strings/Main.java
@@ -1,20 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String command = scanner.nextLine();
+        String input = scanner.nextLine();
+        while (!"end".equals(input)) {
+            StringBuilder reversed = new StringBuilder();
 
-        while (!"end".equals(command)) {
-            String reverse = "";
-            for (int i = command.length() - 1; i >= 0; i--) {
-                reverse += command.charAt(i);
-            }
-            System.out.printf("%s = %s%n", command, reverse);
-            command = scanner.nextLine();
+            reverseString(input, reversed);
+
+            printResult(input, reversed);
+
+            input = scanner.nextLine();
+        }
+    }
+
+    private static void printResult(String input, StringBuilder reversed) {
+        System.out.printf("%s = %s\n", input, reversed);
+    }
+
+    private static void reverseString(String input, StringBuilder reversed) {
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
         }
     }
 }
-
-
