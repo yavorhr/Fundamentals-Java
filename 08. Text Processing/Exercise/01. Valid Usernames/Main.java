5fb6a5107476f6244fc0408977d188cae27f4c03
wolfie <yavorhristozov@gmail.com>
2023-11-21 18:38:55 +0200

    refact : 01. Valid Usernames
    Add methods to split code in Main
    Rename variables
    Change use of for cycle with Stream API

--- 08. Text Processing/Exercise/01. Valid Usernames/Main.java
+++ 08. Text Processing/Exercise/01. Valid Usernames/Main.java
@@ -1,28 +1,47 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] text = scanner.nextLine().split(", ");
-        for (String word : text) {
-            if (wordIsValid(word)){
-                System.out.println(word);
-            }
+        String[] usernames = scanner.nextLine().split(", ");
+
+        String[] filteredUsernames =
+                Arrays.stream(usernames)
+                        .filter(Main::filterValidUsernames)
+                        .toArray(String[]::new);
+
+        printResult(filteredUsernames);
+    }
+
+    private static void printResult(String[] filteredUsernames) {
+        if (filteredUsernames.length > 0) {
+            System.out.println(String.join("\n", filteredUsernames));
         }
     }
 
-    private static boolean wordIsValid(String word) {
-        if (word.length()<3 || word.length()>16){
-            return false;
-        }
+    private static boolean filterValidUsernames(String username) {
+        boolean checkLength = validLength(username);
+        boolean checkCharacters = validCharacters(username);
 
-        for (int i = 0; i <word.length() ; i++) {
-            char currentCh = word.charAt(i);
-            if (!Character.isLetterOrDigit(currentCh) && currentCh != '-' && currentCh != '_'){
+        return checkLength && checkCharacters;
+    }
+
+    private static boolean validCharacters(String username) {
+        for (int i = 0; i < username.length(); i++) {
+            char currentChar = username.charAt(i);
+
+            if (!Character.isDigit(currentChar) && !Character.isLetter(currentChar) && currentChar != '-' && currentChar != '_') {
                 return false;
             }
-        } return true;
+        }
+        return true;
     }
-}
 
+    private static boolean validLength(String username) {
+        return username.length() >= 3 && username.length() <= 16;
+    }
+
+
+}
