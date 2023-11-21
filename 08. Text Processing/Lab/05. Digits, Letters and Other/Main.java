99142521084c1097286e8e04861ff529e55854de
wolfie <yavorhristozov@gmail.com>
2023-11-21 17:53:50 +0200

    refact : 05. Digits, Letters and Other
    Rename variables
    Add methods to split Main logic

--- 08. Text Processing/Lab/05. Digits, Letters and Other/Main.java
+++ 08. Text Processing/Lab/05. Digits, Letters and Other/Main.java
@@ -1,29 +1,36 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String word = scanner.nextLine();
+        String input = scanner.nextLine();
 
         StringBuilder digits = new StringBuilder();
         StringBuilder letters = new StringBuilder();
-        StringBuilder other = new StringBuilder();
+        StringBuilder others = new StringBuilder();
 
-        for (int i = 0; i < word.length(); i++) {
-            char currentChar = word.charAt(i);
-            if (Character.isDigit(currentChar)) {
-                digits.append(currentChar);
-            } else if (Character.isAlphabetic(currentChar)) {
+        addCharToCollection(input, digits, letters, others);
+
+        printResult(digits, letters, others);
+    }
+
+    private static void printResult(StringBuilder digits, StringBuilder letters, StringBuilder others) {
+        String result = String.format("%s\n%s\n%s", digits, letters, others);
+        System.out.println(result);
+    }
+
+    private static void addCharToCollection(String input, StringBuilder digits, StringBuilder letters, StringBuilder others) {
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (Character.isLetter(currentChar)) {
                 letters.append(currentChar);
+            } else if (Character.isDigit(currentChar)) {
+                digits.append(currentChar);
             } else {
-                other.append(currentChar);
+                others.append(currentChar);
             }
         }
-
-        System.out.println(digits);
-        System.out.println(letters);
-        System.out.println(other);
     }
 }
-
