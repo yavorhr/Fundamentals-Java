8b59cd0fa5a68ea8dbd53958aed4d6f9adc6ce0c
wolfie <yavorhristozov@gmail.com>
2023-11-22 11:29:41 +0200

    refact : 06. Replace Repeating Chars
    Add  method for repeating chars
    Rename variables

--- 08. Text Processing/Exercise/06. Replace Repeating Chars/Main.java
+++ 08. Text Processing/Exercise/06. Replace Repeating Chars/Main.java
@@ -1,24 +1,34 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String text = scanner.nextLine();
-        StringBuilder sb = new StringBuilder();
-        char base = text.charAt(0);
-        sb.append(base);
+        String input = scanner.nextLine();
 
-        for (int i = 0; i <text.length() ; i++) {
-            char currentChar = text.charAt(i);
-            if (currentChar!=base){
+        StringBuilder result = new StringBuilder();
+        char base = input.charAt(0);
+        result.append(base);
+
+        replaceRepeatingChars(result,base,input);
+
+        printResult(result);
+    }
+
+    private static void printResult(StringBuilder result) {
+        System.out.println(result);
+    }
+
+    private static void replaceRepeatingChars(StringBuilder sb, char base, String input){
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (currentChar != base) {
                 sb.append(currentChar);
                 base = currentChar;
             }
         }
-        System.out.println(sb);
+
     }
 }
-
-
 
