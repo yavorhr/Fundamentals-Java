ad2820f473dc4dd4fa1d77ecd9419781271bb78d
wolfie <yavorhristozov@gmail.com>
2023-11-22 10:50:23 +0200

    refact : 02. Character Multiplier
    Add methods to split Main
    Rename variables

--- 08. Text Processing/Exercise/02. Character Multiplier/Main.java
+++ 08. Text Processing/Exercise/02. Character Multiplier/Main.java
@@ -1,30 +1,36 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] words = scanner.nextLine().split("\\s+");
+        String[] input = scanner.nextLine().split(" ");
+        String firstWord = input[0];
+        String secondWord = input[1];
 
-        String firstWord = words[0];
-        String secondWord = words[1];
-
-        int min = Math.min(firstWord.length(), secondWord.length());
         int max = Math.max(firstWord.length(), secondWord.length());
+        int min = Math.min(firstWord.length(), secondWord.length());
 
-        int result = 0;
+        int sumChars = 0;
+        sumChars = getSumChars(firstWord, secondWord, max, min, sumChars);
 
+        printSum(sumChars);
+    }
+
+    private static void printSum(int sumChars) {
+        System.out.println(sumChars);
+    }
+
+    private static int getSumChars(String firstWord, String secondWord, int max, int min, int sumChars) {
         for (int i = 0; i < max; i++) {
             if (i < min) {
-                result += firstWord.charAt(i) * secondWord.charAt(i);
+                sumChars += firstWord.charAt(i) * secondWord.charAt(i);
             } else if (firstWord.length() == max){
-                result += firstWord.charAt(i);
+                sumChars += firstWord.charAt(i);
             } else {
-                result += secondWord.charAt(i);
+                sumChars += secondWord.charAt(i);
             }
         }
-
-        System.out.println(result);
+        return sumChars;
     }
 }
-
