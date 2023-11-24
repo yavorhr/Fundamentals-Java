98355bf7dfdb45e79a651b41bd26ea497c42eb34
wolfie <yavorhristozov@gmail.com>
2023-11-24 17:00:52 +0200

    refact : 03. Matches Dates
    Add print method for the dates

--- 09. Regular Expressions/Lab/03. Match Dates/Main.java
+++ 09. Regular Expressions/Lab/03. Match Dates/Main.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,13 +8,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String text = scanner.nextLine();
-        String regex = "\\b(?<day>\\d{2})([/\\-.])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})";
+        String input = scanner.nextLine();
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(text);
+        Pattern regex = Pattern.compile("\\b(?<day>\\d{2})([\\/.-])(?<month>[A-z][a-z]{2})\\2(?<year>\\d{4})\\b");
+        Matcher matcher = regex.matcher(input);
 
-        while (matcher.find()){
+        printResult(matcher);
+    }
+
+    private static void printResult(Matcher matcher) {
+        while (matcher.find()) {
             String day = matcher.group("day");
             String month = matcher.group("month");
             String year = matcher.group("year");
@@ -20,5 +25,6 @@ public class Main {
             System.out.printf("Day: %s, Month: %s, Year: %s%n",day,month,year);
         }
     }
-}
 
+
+}
