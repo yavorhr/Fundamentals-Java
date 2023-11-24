11fc40fa9ae063a35bfd61172dbe99626400399e
wolfie <yavorhristozov@gmail.com>
2023-11-24 15:40:05 +0200

    refact : 02. Match Phone Number
    Split Main code by extracting methods for better readability

--- 09. Regular Expressions/Lab/02. Match Phone Number/Main.java
+++ 09. Regular Expressions/Lab/02. Match Phone Number/Main.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,19 +8,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String phone = scanner.nextLine();
-        String regex = "\\+359([ -])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+        String input = scanner.nextLine();
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(phone);
+        Pattern regex = Pattern.compile("\\+359([ -])2\\1[\\d]{3}\\1[\\d]{4}\\b");
+        Matcher matcher = regex.matcher(input);
 
-        List <String> matches = new ArrayList<>();
+        List<String> phoneNumbers = addPhoneNumbers(matcher);
+        printResult(phoneNumbers);
+    }
 
-        while (matcher.find()){
-            matches.add(matcher.group());
+    private static void printResult(List<String> phoneNumbers) {
+        System.out.println(String.join(", ", phoneNumbers));
+    }
+
+    private static List<String> addPhoneNumbers(Matcher matcher) {
+
+        List<String> phoneNumbers = new ArrayList<>();
+
+        while (matcher.find()) {
+            phoneNumbers.add(matcher.group());
         }
-
-        System.out.println(String.join(", ",matches));
+        return phoneNumbers;
     }
 }
-
