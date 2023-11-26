3a82f57f22fbb3651125a548b7e690de211f15e5
wolfie <yavorhristozov@gmail.com>
2023-11-26 14:20:03 +0200

    refact : 01. Disneyland Journey
    Add methods to split  Main
    Rename variables

--- 11. Exams practice/Mid Exams/Mid Exam Retake - 10 December 2019/01. Disneyland Journey/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam Retake - 10 December 2019/01. Disneyland Journey/Main.java
@@ -4,40 +4,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double journeyCost = Double.parseDouble(scanner.nextLine());
+        double budget = Double.parseDouble(scanner.nextLine());
         int months = Integer.parseInt(scanner.nextLine());
 
-        double savings = 0;
+        double savings = calcSavings(budget, months);
+
+        printResult(savings,budget);
+    }
+
+    private static double calcSavings(double budget, int months) {
+        double savings = 0.0;
+
         for (int i = 1; i <= months; i++) {
+
             if (i != 1 && i % 2 != 0) {
-                savings = savings - 0.16 * savings;
+                savings *= 0.84;
             }
 
             if (i % 4 == 0) {
-                savings = savings + savings * 0.25;
+                savings *= 1.25;
             }
-            savings += 0.25 * journeyCost;
+            savings += budget * 0.25;
+        }
+        return savings;
+    }
+
+    private static void printResult(double savings, double budget){
+        double diff = Math.abs(savings - budget);
+
+        if (savings >= budget) {
+            System.out.printf("Bravo! You can go to Disneyland and you will have %.2flv. for souvenirs.\n", diff);
+        } else {
+            System.out.printf("Sorry. You need %.2flv. more.",diff);
         }
 
-        if (savings >= journeyCost) {
-            System.out.printf("Bravo! You can go to Disneyland and you will have %.2flv. for souvenirs.", savings - journeyCost);
-        } else {
-            System.out.printf("Sorry. You need %.2flv. more.", journeyCost - savings);
-        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
