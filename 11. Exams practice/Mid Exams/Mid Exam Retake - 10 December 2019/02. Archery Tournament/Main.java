96f2f197d7a675dd043ab75eae1564a9af4ab61c
wolfie <yavorhristozov@gmail.com>
2023-11-27 16:58:05 +0200

    refact : 02. Archery Tournament
    Add methods to avoid repeating code and for better readability
    Rename variables

--- 11. Exams practice/Mid Exams/Mid Exam Retake - 10 December 2019/02. Archery Tournament/Main.java
+++ 11. Exams practice/Mid Exams/Mid Exam Retake - 10 December 2019/02. Archery Tournament/Main.java
@@ -1,91 +1,97 @@
 import java.util.*;
 
 public class Main {
+    private final static int ARROW_POINTS = 5;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersAstext = scanner.nextLine().split("\\|");
-        int targetsNumbers[] = new int[numbersAstext.length];
-
-        for (int i = 0; i < numbersAstext.length; i++) {
-            targetsNumbers[i] = Integer.parseInt(numbersAstext[i]);
-        }
+        int[] targets =
+                Arrays.stream(scanner.nextLine()
+                        .split("\\|"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
 
         int sumPoints = 0;
 
         String input = scanner.nextLine();
-        while (!"Game over".equals(input)) {
-            String[] token = input.split("\\@");
-            String direction = token[0];
-            if (direction.equals("Shoot Left")) {
-                int startIndexLeft = Integer.parseInt(token[1]);
-                int lengthLeft = Integer.parseInt(token[2]);
-                if (startIndexLeft >= 0 && startIndexLeft < numbersAstext.length) {
-                    int targetIndex = startIndexLeft - lengthLeft;
-                    while (targetIndex < 0) {
-                        targetIndex = targetsNumbers.length + targetIndex;
-                    }
 
-                    if (targetsNumbers[targetIndex] > 5) {
-                        targetsNumbers[targetIndex] = targetsNumbers[targetIndex] - 5;
-                        sumPoints += 5;
-                    } else if (targetsNumbers[targetIndex] < 5) {
-                        sumPoints += targetsNumbers[targetIndex];
-                        targetsNumbers[targetIndex] = 0;
+        while (!"Game over".equals(input)) {
+            String[] tokens = input.split("@");
+            String command = tokens[0];
+
+            switch (command) {
+                case "Shoot Left" -> {
+                    int startIndexLeft = Integer.parseInt(tokens[1]);
+                    int lengthLeft = Integer.parseInt(tokens[2]);
+
+                    if (isIndexValid(targets, startIndexLeft)) {
+                        int targetIndex = getIndexToTheLeft(targets, startIndexLeft, lengthLeft);
+                        sumPoints = addTargetPoints(targets, sumPoints, targetIndex);
                     }
                 }
-            } else if (direction.equals("Shoot Right")) {
-                int startIndexRight = Integer.parseInt(token[1]);
-                int lengthRight = Integer.parseInt(token[2]);
-                if (startIndexRight >= 0 && startIndexRight < numbersAstext.length) {
-                    int targetIndex = startIndexRight + lengthRight;
-                    while (targetIndex >= targetsNumbers.length) {
-                        targetIndex -= targetsNumbers.length;
-                    }
-                    if (targetsNumbers[targetIndex] > 5) {
-                        targetsNumbers[targetIndex] -= 5;
-                        sumPoints += 5;
-                    } else if (targetsNumbers[targetIndex] < 5) {
-                        sumPoints += targetsNumbers[targetIndex];
-                        targetsNumbers[targetIndex] = 0;
+                case "Shoot Right" -> {
+                    int startIndexRight = Integer.parseInt(tokens[1]);
+                    int lengthRight = Integer.parseInt(tokens[2]);
+
+                    if (isIndexValid(targets, startIndexRight)) {
+                        int targetIndex = startIndexRight + lengthRight;
+                        targetIndex = getIndexToTheRight(targets, targetIndex);
+                        sumPoints = addTargetPoints(targets, sumPoints, targetIndex);
                     }
                 }
-            } else if (direction.equals("Reverse")) {
-                reverse(targetsNumbers);
+                case "Reverse" -> reverse(targets);
             }
             input = scanner.nextLine();
         }
 
-        for (int i = 0; i < targetsNumbers.length - 1; i++) {
-            System.out.print(targetsNumbers[i] + " - ");
-        }
-
-        System.out.println(targetsNumbers[targetsNumbers.length - 1]);
-        System.out.printf("Iskren finished the archery tournament with %d points!%n", sumPoints);
+        printResult(targets, sumPoints);
     }
 
-    private static int[] reverse(int[] targetsNumbers) {
-        for (int i = 0; i < targetsNumbers.length / 2; i++) {
-            int temp = targetsNumbers[i];
-            int swapIndex = targetsNumbers.length - 1 - i;
-            targetsNumbers[i] = targetsNumbers[swapIndex];
-            targetsNumbers[swapIndex] = temp;
+    private static void printResult(int[] targets, int sumPoints) {
+        Arrays.stream(targets).forEach(t -> System.out.print(t + " "));
+        System.out.println();
+        System.out.printf("Iskren finished the archery tournament with %d points!", sumPoints);
+    }
+
+    private static int getIndexToTheRight(int[] targets, int targetIndex) {
+        while (targetIndex >= targets.length) {
+            targetIndex -= targets.length;
         }
-        return targetsNumbers;
+        return targetIndex;
+    }
+
+    private static int addTargetPoints(int[] targets, int sumPoints, int targetIndex) {
+        if (targets[targetIndex] >= ARROW_POINTS) {
+            targets[targetIndex] -= ARROW_POINTS;
+            sumPoints += 5;
+        } else if (targets[targetIndex] < ARROW_POINTS) {
+            sumPoints += targets[targetIndex];
+            targets[targetIndex] = 0;
+        }
+        return sumPoints;
+    }
+
+    private static int getIndexToTheLeft(int[] targets, int startIndexLeft, int lengthLeft) {
+        int targetIndex = startIndexLeft - lengthLeft;
+        while (targetIndex < 0) {
+            targetIndex = targets.length + targetIndex;
+        }
+        return targetIndex;
+    }
+
+    private static boolean isIndexValid(int[] targets, int startIndexLeft) {
+        return startIndexLeft >= 0 && startIndexLeft < targets.length;
+    }
+
+    private static void reverse(int[] targets) {
+        for (int i = 0; i < targets.length / 2; i++) {
+            int temp = targets[i];
+            int swapIndex = targets.length - 1 - i;
+            targets[i] = targets[swapIndex];
+            targets[swapIndex] = temp;
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
