b8c85df54d8c4f9d1a4304f1344cb45afb459bdd
wolfie <yavorhristozov@gmail.com>
2023-11-28 16:59:55 +0200

    refact : 01. Easter Cozonacs
    Add methods to split Main
    Rename variables

--- 11. Exams practice/Mid Exams/Retake Mid Exam - 16 April 2019/01. Easter Cozonacs/Main.java
+++ 11. Exams practice/Mid Exams/Retake Mid Exam - 16 April 2019/01. Easter Cozonacs/Main.java
@@ -9,57 +9,38 @@ public class Main {
 
         double price1packEggs = price1kgFloor * 0.75;
         double milkPrice = price1kgFloor * 1.25;
-        double milkPriceKozunac = milkPrice * 0.25;
+        double milkPriceCozonac = milkPrice * 0.25;
 
-        double kozunacCost = price1kgFloor + price1packEggs + milkPriceKozunac;
-        int kozunacsCount = 0;
+        double cozonacsCost = getCozonacsCost(price1kgFloor, price1packEggs, milkPriceCozonac);
+
+        int cozonacsCount = 0;
         int coloredEggs = 0;
 
-        while (budget > kozunacCost ) {
-            budget -= kozunacCost;
-            kozunacsCount++;
+        while (budgetIsEnough(budget, cozonacsCost)) {
+            budget -= cozonacsCost;
+            cozonacsCount++;
             coloredEggs += 3;
-            if (kozunacsCount % 3 == 0) {
-                int lostEggs = kozunacsCount - 2;
+
+            if (cozonacsCount % 3 == 0) {
+                int lostEggs = cozonacsCount - 2;
                 coloredEggs -= lostEggs;
             }
-
         }
-        System.out.printf("You made %d cozonacs! Now you have %d eggs and %.2fBGN left.", kozunacsCount, coloredEggs, budget);
+
+        printResult(budget, cozonacsCount, coloredEggs);
+    }
+
+    private static void printResult(double budget, int cozonacsCount, int coloredEggs) {
+        System.out.printf("You made %d cozonacs! Now you have %d eggs and %.2fBGN left.", cozonacsCount, coloredEggs, budget);
+    }
+
+    private static boolean budgetIsEnough(double budget, double cozonacsCost) {
+        return budget >= cozonacsCost;
+    }
+
+    private static double getCozonacsCost(double price1kgFloor, double price1packEggs, double milkPriceCozonac) {
+        return price1kgFloor + price1packEggs + milkPriceCozonac;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
