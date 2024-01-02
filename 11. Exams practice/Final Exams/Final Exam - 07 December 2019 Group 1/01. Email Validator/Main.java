771b72b5f45a9a302f8469cd78b8df49821f9f8e
wolfie <yavorhristozov@gmail.com>
2024-01-02 19:31:47 +0200

    refact : 01. Final Exam 07 December 2019 Group 1
    Re-write the task by extracting simple methods and reduce some operations code complexity
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam - 07 December 2019 Group 1/01. Email Validator/Main.java
+++ 11. Exams practice/Final Exams/Final Exam - 07 December 2019 Group 1/01. Email Validator/Main.java
@@ -12,52 +12,54 @@ public class Main {
             String command = tokens[0];
 
             switch (command) {
-                case "Make":
-                    String upperOrLower = tokens[1];
-                    if (upperOrLower.equals("Upper")) {
+                case "Make" -> {
+                    String criteria = tokens[1];
+                    if (criteria.equals("Upper")) {
                         email = email.toUpperCase();
-                        System.out.println(email);
-                    } else {
+                        printEmail(email);
+                    } else if (criteria.equals("Lower")) {
                         email = email.toLowerCase();
-                        System.out.println(email);
+                        printEmail(email);
                     }
-                    break;
-                case "GetDomain":
+                }
+                case "GetDomain" -> {
                     int count = Integer.parseInt(tokens[1]);
-                    String domain = email.substring(email.length() - count);
-                    System.out.println(domain);
-                    break;
-                case "GetUsername":
-                    if (email.contains("@")) {
-                        int end = email.indexOf('@');
-                        String userName = email.substring(0, end);
-                        System.out.println(userName);
-                    } else {
-                        System.out.println(String.format("The email %s doesn't contain the @ symbol.", email));
-                    }
-                    break;
-                case "Replace":
-                    String toReplace = tokens[1];
-                    if (email.contains(toReplace)) {
-                        email = email.replace(toReplace, "-");
-                    }
-                    System.out.println(email);
-                    break;
-                case "Encrypt":
-                    for (int i = 0; i < email.length()-1; i++) {
-                        char currChar = email.charAt(i);
-                        System.out.print(+currChar + " ");
-                    }
-                    System.out.print(+email.charAt(email.length()-1));
+                    int startIndex = email.length() - count;
+                    String subString = email.substring(startIndex);
 
-                    break;
+                    printEmail(subString);
+                }
+                case "GetUsername" -> {
+                    int endIndex = email.indexOf("@");
+
+                    if (endIndex == -1) {
+                        System.out.printf("The email %s doesn't contain the @ symbol.\n", email);
+                    } else {
+                        String substring = email.substring(0, endIndex);
+                        printEmail(substring);
+                    }
+                }
+                case "Replace" -> {
+                    char character = tokens[1].charAt(0);
+
+                    if (email.contains(character + "")) {
+                        email = email.replace(character, '-');
+                        printEmail(email);
+                    }
+                }
+                case "Encrypt" -> {
+                    for (char symbol : email.toCharArray()) {
+                        System.out.print(+symbol + " ");
+                    }
+                }
             }
             input = scanner.nextLine();
         }
     }
+
+    private static void printEmail(String email) {
+        System.out.println(email);
+    }
 }
-
-
-
 
 
