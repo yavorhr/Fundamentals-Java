1df9920a8cf50613f26bbc4b49f731d2cecb069d
wolfie <yavorhristozov@gmail.com>
2024-01-03 20:51:32 +0200

    refact : 01. Nikulden's Charity
    Re-write the task by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the code more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam - 07 December 2019 Group 2/01. Nikulden’s Charity/Main.java
+++ 11. Exams practice/Final Exams/Final Exam - 07 December 2019 Group 2/01. Nikulden’s Charity/Main.java
@@ -4,72 +4,92 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String charityAmount = scanner.nextLine();
-
+        String string = scanner.nextLine();
         String input = scanner.nextLine();
         while (!"Finish".equals(input)) {
-            String[] parts = input.split(" ");
-            String command = parts[0];
+            String[] tokens = input.split(" ");
+            String command = tokens[0];
 
             switch (command) {
-                case "Replace":
-                    String currentChar = parts[1];
-                    String newChar = parts[2];
+                case "Replace" -> {
+                    char oldChar = tokens[1].charAt(0);
+                    char newChar = tokens[2].charAt(0);
 
-                    charityAmount = charityAmount.replace(currentChar, newChar);
-                    System.out.println(charityAmount);
-                    break;
-                case "Cut":
-                    int startIndex = Integer.parseInt(parts[1]);
-                    int endIndex = Integer.parseInt(parts[2]);
+                    string = string.replace(oldChar, newChar);
+                    printCurrentString(string);
+                }
+                case "Cut" -> {
+                    int startIndex = Integer.parseInt(tokens[1]);
+                    int endIndex = Integer.parseInt(tokens[2]);
 
-                    if (startIndex >= 0 && endIndex < charityAmount.length()) {
-                        String substring = charityAmount.substring(startIndex, endIndex + 1);
-                        charityAmount = charityAmount.replace(substring, "");
-                        System.out.println(charityAmount);
+                    if (indexIsValid(startIndex, string) && indexIsValid(endIndex, string)) {
+                        String substring = getSubstring(string, startIndex, endIndex);
+                        string = string.replace(substring, "");
+
+                        printCurrentString(string);
                     } else {
                         System.out.println("Invalid indexes!");
                     }
-                    break;
-                case "Make":
-                    String upperOrLower = parts[1];
-                    if (upperOrLower.equals("Upper")) {
-                        charityAmount = charityAmount.toUpperCase();
-                        System.out.println(charityAmount);
-                    } else if (upperOrLower.equals("Lower")) {
-                        charityAmount = charityAmount.toLowerCase();
-                        System.out.println(charityAmount);
+                }
+                case "Make" -> {
+                    String criteria = tokens[1];
+
+                    if (criteria.equals("Lower")) {
+                        string = string.toLowerCase();
+                    } else if (criteria.equals("Upper")) {
+                        string = string.toUpperCase();
                     }
-                    break;
-                case "Check":
-                    String string = parts[1];
-                    if (charityAmount.contains(string)) {
-                        System.out.println(String.format("Message contains %s", string));
+                    printCurrentString(string);
+                }
+                case "Check" -> {
+                    String charSequence = tokens[1];
+                    if (string.contains(charSequence)) {
+                        printMessage("Message contains ", charSequence);
                     } else {
-                        System.out.println(String.format("Message doesn't contain %s", string));
+                        printMessage("Message doesn't contain ", charSequence);
                     }
-                    break;
-                case "Sum":
-                    startIndex = Integer.parseInt(parts[1]);
-                    endIndex = Integer.parseInt(parts[2]);
-                    if (startIndex >= 0 && endIndex < charityAmount.length()) {
-                        int sum = 0;
-                        String substring = charityAmount.substring(startIndex, endIndex+ 1);
-                        for (int i = 0; i < substring.length(); i++) {
-                            int currChar = substring.charAt(i);
-                            sum += currChar;
-                        }
+                }
+                case "Sum" -> {
+                    int startIndex = Integer.parseInt(tokens[1]);
+                    int endIndex = Integer.parseInt(tokens[2]);
+
+                    if (indexIsValid(startIndex, string) && indexIsValid(endIndex, string)) {
+                        String substring = getSubstring(string, startIndex, endIndex);
+
+                        int sum = getSum(substring);
+
                         System.out.println(sum);
                     } else {
                         System.out.println("Invalid indexes!");
                     }
-                    break;
+                }
             }
             input = scanner.nextLine();
         }
     }
+
+    private static int getSum(String substring) {
+        int sum = 0;
+        for (char character : substring.toCharArray()) {
+            sum += character;
+        }
+        return sum;
+    }
+
+    private static void printCurrentString(String string) {
+        System.out.println(string);
+    }
+
+    private static String getSubstring(String string, int startIndex, int endIndex) {
+        return string.substring(startIndex, endIndex + 1);
+    }
+
+    private static void printMessage(String message, String charSequence) {
+        System.out.println(message + charSequence);
+    }
+
+    private static boolean indexIsValid(int index, String string) {
+        return index > 0 && index < string.length();
+    }
 }
-
-
-
 
