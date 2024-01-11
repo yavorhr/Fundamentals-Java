e30bab0af4c4c45692217758eee2cdc5cc754737
wolfie <yavorhristozov@gmail.com>
2024-01-11 07:35:54 +0200

    refact : 01. Arriving in Kathmandu
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam - 14 April 2019 Group I/01. Arriving in Kathmandu/Main.java
+++ 11. Exams practice/Final Exams/Final Exam - 14 April 2019 Group I/01. Arriving in Kathmandu/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,27 +12,48 @@ public class Main {
         String input = scanner.nextLine();
         while (!"Last note".equals(input)) {
             Matcher matcher = pattern.matcher(input);
-            if (matcher.find() && Integer.parseInt(matcher.group("length")) == matcher.group("code").length()) {
+
+            if (matcher.find()) {
                 String name = matcher.group("name");
+                int length = Integer.parseInt(matcher.group("length"));
                 String code = matcher.group("code");
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < name.length(); i++) {
-                    char currentChar = name.charAt(i);
-                    if (Character.isLetter(currentChar)) {
-                        sb.append(currentChar);
-                    }
+
+                if (length != code.length()) {
+                    printNotFound();
+                    input = scanner.nextLine();
+                    continue;
                 }
-                System.out.println(String.format("Coordinates found! %s -> %s", sb.toString(), code));
+                String outputName = decypherName(name);
+                printResult(code, outputName);
+
             } else {
-                System.out.println("Nothing found!");
+                printNotFound();
             }
+
             input = scanner.nextLine();
         }
     }
+
+    private static void printResult(String code, String outputName) {
+        System.out.printf("Coordinates found!%s -> %s\n", outputName, code);
+    }
+
+    private static void printNotFound() {
+        System.out.println("Nothing found!");
+    }
+
+    private static String decypherName(String name) {
+        StringBuilder sb = new StringBuilder();
+        String regex = "[A-Za-z]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+
+        while (matcher.find()){
+            sb.append(matcher.group());
+        }
+
+        return sb.toString();
+    }
 }
-
-
-
-
-
 
