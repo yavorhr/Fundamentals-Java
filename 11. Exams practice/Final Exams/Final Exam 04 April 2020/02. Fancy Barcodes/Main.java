13b142a80a32a3b551373ad35d96728d44eefb38
wolfie <yavorhristozov@gmail.com>
2024-01-23 11:24:03 +0200

    refact : 02. Fancy Barcodes
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam 04 April 2020/02. Fancy Barcodes/Main.java
+++ 11. Exams practice/Final Exams/Final Exam 04 April 2020/02. Fancy Barcodes/Main.java
@@ -2,40 +2,47 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String regex = "@#*[A-Z][A-Za-z|0-9]{4,}[A-Z]@#+";
-        Pattern pattern = Pattern.compile(regex);
-
         int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String barcode = scanner.nextLine();
-            Matcher matcher = pattern.matcher(barcode);
+
+        String regex = "(@#+)(?<barcode>[A-Z][A-Za-z|0-9]{4,}[A-Z])\\1";
+        Pattern pattern = Pattern.compile(regex);
+        StringBuilder result = new StringBuilder();
+
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
 
             if (matcher.find()) {
-                StringBuilder sb = new StringBuilder();
-
-                String currentBarCode = matcher.group();
-                for (int j = 0; j < currentBarCode.length(); j++) {
-                    char currentChar = currentBarCode.charAt(j);
-                    if (Character.isDigit(currentChar)) {
-                        sb.append(currentChar);
-                    }
-                }
-
-                if (sb.toString().isEmpty()) {
-                    System.out.println(String.format("Product group: 00"));
-                } else {
-                    System.out.println(String.format("Product group: %s",sb.toString()));
-                }
+                String barcode = matcher.group("barcode");
+                result.append(getProductGroup(barcode)).append(System.lineSeparator());
             } else {
-                System.out.println("Invalid barcode");
+                result.append("Invalid barcode\n");
             }
-
         }
+
+        System.out.println(result);
+    }
+
+    private static String getProductGroup(String barcode) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < barcode.length(); i++) {
+            char currentChar = barcode.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                sb.append(currentChar);
+            }
+        }
+
+        return sb.length() == 0
+                ? "Product group: 00"
+                : String.format("Product group: %s", sb);
     }
 }
+
+
+
 
