d6a0a63e26fac8b4ac433349b6dfd94d06592746
wolfie <yavorhristozov@gmail.com>
2024-01-23 15:02:37 +0200

    refact : 03.  Heroes of Code and Logic VII
    Re-write the program by extracting simple methods, add Hero class, rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam 04 April 2020/03. Heroes of Code and Logic VII/Main.java
+++ 11. Exams practice/Final Exams/Final Exam 04 April 2020/03. Heroes of Code and Logic VII/Main.java
@@ -4,94 +4,85 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> mapHealthPoints = new LinkedHashMap<>();
-        Map<String, Integer> mapManaPoints = new LinkedHashMap<>();
+        Map<String, Hero> heroes = new HashMap<>();
 
-        int heroesCount = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < heroesCount; i++) {
-            String[] tokens = scanner.nextLine().split("\\s+");
-            String nameOfHero = tokens[0];
-            int healthPoints = Integer.parseInt(tokens[1]);
-            int manaPoints = Integer.parseInt(tokens[2]);
+        int n = Integer.parseInt(scanner.nextLine());
 
-            mapHealthPoints.put(nameOfHero, healthPoints);
-            mapManaPoints.put(nameOfHero, manaPoints);
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+            addHeroToList(input, heroes);
         }
+
 
         String input = scanner.nextLine();
         while (!"End".equals(input)) {
             String[] tokens = input.split(" - ");
             String command = tokens[0];
-            String currentHero = tokens[1];
+            String heroName = tokens[1];
 
             switch (command) {
-                case "CastSpell":
-                    int manaNeeded = Integer.parseInt(tokens[2]);
-                    String spellName = tokens[3];
+                case "Heal" -> {
+                    int hp = Integer.parseInt(tokens[2]);
 
-                    if (mapManaPoints.get(currentHero) >= manaNeeded) {
-                        int newMana = mapManaPoints.get(currentHero) - manaNeeded;
-                        mapManaPoints.put(currentHero, newMana);
-                        System.out.println(String.format("%s has successfully cast %s and now has %d MP!", currentHero, spellName, newMana));
-                    } else {
-                        System.out.println(String.format("%s does not have enough MP to cast %s!", currentHero, spellName));
-                    }
-                    break;
-                case "TakeDamage":
+                    Hero currentHero = heroes.get(heroName);
+                    currentHero.heal(hp);
+                    heroes.put(heroName, currentHero);
+                }
+                case "Recharge" -> {
+                    int mp = Integer.parseInt(tokens[2]);
+
+                    Hero currentHero = heroes.get(heroName);
+                    currentHero.recharge(mp);
+                    heroes.put(heroName, currentHero);
+                }
+                case "TakeDamage" -> {
                     int damage = Integer.parseInt(tokens[2]);
                     String attacker = tokens[3];
 
-                    if (mapHealthPoints.get(currentHero) - damage > 0) {
-                        int newNealthPoints = mapHealthPoints.get(currentHero) - damage;
-                        mapHealthPoints.put(currentHero, newNealthPoints);
-                        System.out.println(String.format("%s was hit for %d HP by %s and now has %d HP left!", currentHero, damage, attacker, mapHealthPoints.get(currentHero)));
-                    } else {
-                        mapHealthPoints.remove(currentHero);
-                        mapManaPoints.remove(currentHero);
-                        System.out.println(String.format("%s has been killed by %s!", currentHero, attacker));
+                    Hero currentHero = heroes.get(heroName);
+                    boolean heroIsKilled = currentHero.takeDamage(damage, attacker);
+
+                    if (heroIsKilled) {
+                        heroes.remove(heroName);
                     }
-                    break;
-                case "Recharge":
-                    int amountMana = Integer.parseInt(tokens[2]);
-                    if (mapManaPoints.get(currentHero) + amountMana > 200) {
-                        int oldMana = mapManaPoints.get(currentHero);
-                        mapManaPoints.put(currentHero,200);
-                        System.out.println(String.format("%s recharged for %d MP!",currentHero,200-oldMana));
-                    } else {
-                        int newMana = mapManaPoints.get(currentHero)+amountMana;
-                        mapManaPoints.put(currentHero,newMana);
-                        System.out.println(String.format("%s recharged for %d MP!",currentHero,amountMana));
-                    }
-                    break;
-                case "Heal":
-                    int amountHealth = Integer.parseInt(tokens[2]);
-                    if (mapHealthPoints.get(currentHero) + amountHealth > 100) {
-                        int oldHealth = mapHealthPoints.get(currentHero);
-                        mapHealthPoints.put(currentHero,100);
-                        System.out.println(String.format("%s healed for %d HP!",currentHero,100-oldHealth));
-                    } else {
-                        int newHealth = mapHealthPoints.get(currentHero)+amountHealth;
-                        mapHealthPoints.put(currentHero,newHealth);
-                        System.out.println(String.format("%s healed for %d HP!",currentHero,amountHealth));
-                    }
-                    break;
+                }
+                case "CastSpell" -> {
+                    int mana = Integer.parseInt(tokens[2]);
+                    String spellName = tokens[3];
+
+                    Hero currentHero = heroes.get(heroName);
+                    currentHero.castSpell(mana, spellName);
+                    heroes.put(heroName, currentHero);
+                }
             }
             input = scanner.nextLine();
         }
-        mapHealthPoints
-                .entrySet()
-                .stream()
-                .sorted((h1,h2)-> {
-                    int result = Integer.compare(h2.getValue(),h1.getValue());
-                    if (result==0){
-                        result = h1.getKey().compareTo(h2.getKey());
-                    }return result;
-                }).forEach(h->{
-            System.out.println(String.format("%s",h.getKey()));
-            System.out.println(String.format("  HP: %d",h.getValue()));
-            System.out.println(String.format("  MP: %d",mapManaPoints.get(h.getKey())));
+        printResult(heroes);
+    }
+
+    private static void printResult(Map<String, Hero> heroes) {
+        StringBuilder sb = new StringBuilder();
+        heroes.values().stream().sorted().forEach(h -> {
+            sb.append(String.format("""
+                    %s
+                      HP: %d
+                      MP: %d
+                    """, h.getName(), h.getHealth(), h.getMana()));
         });
+
+        System.out.println(sb.toString());
+    }
+
+    private static void addHeroToList(String input, Map<String, Hero> heroes) {
+        String[] heroTokens = input.split(" ");
+        String name = heroTokens[0];
+        int hp = Integer.parseInt(heroTokens[1]);
+        int mana = Integer.parseInt(heroTokens[2]);
+
+        heroes.put(name, new Hero(name, hp, mana));
     }
 }
+
+
 
 
