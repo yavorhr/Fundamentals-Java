ac87bb27df9e60998b797c750030dce320119e64
wolfie <yavorhristozov@gmail.com>
2024-01-23 16:41:43 +0200

    refact : 02 Boss Rush
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam 13 December 2019/02. Boss Rush/Main.java
+++ 11. Exams practice/Final Exams/Final Exam 13 December 2019/02. Boss Rush/Main.java
@@ -6,23 +6,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
         String regex = "\\|(?<name>[A-Z]{4,})\\|:#(?<title>[A-Za-z]+\\s[A-Za-z]+)#";
         Pattern pattern = Pattern.compile(regex);
-        int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String input = scanner.nextLine();
-            Matcher matcher = pattern.matcher(input);
 
-            if (matcher.find()){
-                String name = matcher.group("name");
-                String title = matcher.group("title");
-                System.out.println(String.format("%s, The %s",name,title));
-                System.out.println(String.format(">> Strength: %d",name.length()));
-                System.out.println(String.format(">> Armour: %d",title.length()));
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+
+            Matcher matcher = pattern.matcher(input);
+            if (!matcher.find()) {
+                printMessage("Access denied!");
             } else {
-                System.out.println("Access denied!");
+                String bossData = getBossData(matcher);
+                printMessage(bossData);
             }
         }
     }
+
+    private static String getBossData(Matcher matcher) {
+        String name = matcher.group("name");
+        String title = matcher.group("title");
+
+        return String.format("%s, %s\n>> Strength: %d\n>> Armour: %d"
+                , name, title, name.length(), title.length());
+    }
+
+    private static void printMessage(String msg) {
+        System.out.println(msg);
+    }
 }
+
+
+
 
