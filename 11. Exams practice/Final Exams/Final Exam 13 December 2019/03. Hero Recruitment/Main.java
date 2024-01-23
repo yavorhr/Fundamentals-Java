a995a2bfe3dab6eae7f466e61473a1b254e2f643
wolfie <yavorhristozov@gmail.com>
2024-01-23 19:01:09 +0200

    refact : 03. Hero Recruitment - Main
    Re-write the task with classes, instead of using linear data structures.
    This is for practice purpose - previous resolution gives 100/100 in judge system.

--- 11. Exams practice/Final Exams/Final Exam 13 December 2019/03. Hero Recruitment/Main.java
+++ 11. Exams practice/Final Exams/Final Exam 13 December 2019/03. Hero Recruitment/Main.java
@@ -4,77 +4,56 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, List<String>> heroes = new LinkedHashMap<>();
+        Tavern tavern = new Tavern();
 
         String input = scanner.nextLine();
         while (!"End".equals(input)) {
-            String[] tokens = input.split("\\s+");
+            String[] tokens = input.split(" ");
             String command = tokens[0];
-            String currentHero = tokens[1];
+            String name = tokens[1];
 
             switch (command) {
-                case "Enroll":
-                    if (heroes.containsKey(currentHero)) {
-                        System.out.printf("%s is already enrolled.%n", currentHero);
+                case "Enroll" -> {
+                    if (tavern.heroExists(name)) {
+                        printErrorMessage(name, " Stefan is already enrolled");
                     } else {
-                        heroes.putIfAbsent(currentHero, new ArrayList<>());
+                        tavern.enroll(new Hero(name));
                     }
-                    break;
-                case "Learn":
+                }
+                case "Unlearn" -> {
                     String spellName = tokens[2];
 
-                    if (!heroes.containsKey(currentHero)) {
-                        System.out.printf("%s doesn't exist.%n", currentHero);
-                        break;
-                    }
-
-                    List<String> currentSpells = heroes.get(currentHero);
-                    if (currentSpells.contains(spellName)) {
-                        System.out.printf("%s has already learnt %s.%n", currentHero, spellName);
+                    if (tavern.heroExists(name)) {
+                        Hero hero = tavern.findHero(name);
+                        hero.unlearn(spellName);
+                        tavern.updateTavern(hero);
                     } else {
-                        List<String> addSpells = heroes.get(currentHero);
-                        addSpells.add(spellName);
-                        heroes.put(currentHero, addSpells);
+                        printErrorMessage(name, " doesn't exist.");
                     }
-                    break;
-                case "Unlearn":
-                    spellName = tokens[2];
-                    if (!heroes.containsKey(currentHero)) {
-                        System.out.printf("%s doesn't exist.%n", currentHero);
-                        break;
-                    }
+                }
+                case "Learn" -> {
+                    String spellName = tokens[2];
 
-                    List<String> spellsList = heroes.get(currentHero);
-                    if (!spellsList.contains(spellName)) {
-                        System.out.println(String.format("%s doesn't know %s.", currentHero, spellName));
-                        break;
+                    if (tavern.heroExists(name)) {
+                        Hero hero = tavern.findHero(name);
+                        hero.learn(spellName);
+                        tavern.updateTavern(hero);
                     } else {
-                        spellsList.remove(spellName);
-                        heroes.put(currentHero, spellsList);
+                        printErrorMessage(name, " doesn't exist.");
                     }
-                    break;
+                }
             }
-
             input = scanner.nextLine();
         }
-        System.out.println("Heroes:");
-        heroes
-                .entrySet()
-                .stream()
-                .sorted((h1, h2) -> {
-                    int result = Integer.compare(h2.getValue().size(), h1.getValue().size());
-                    if (result == 0) {
-                        result = h1.getKey().compareTo(h2.getKey());
-                    }
-                    return result;
-                })
-                .forEach(entry -> {
-                    System.out.printf("== %s: ", entry.getKey());
-                    System.out.printf("%s", String.join(", ", entry.getValue()));
-                    System.out.println();
-                });
+
+        System.out.println(tavern);
+    }
+
+    private static void printErrorMessage(String name, String msg) {
+        System.out.printf("%s %s\n", name, msg);
     }
 }
+
 
 
 
