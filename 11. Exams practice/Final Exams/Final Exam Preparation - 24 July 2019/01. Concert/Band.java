cc3bbd23b34d2ea60b45ce5e442619181e9490db
wolfie <yavorhristozov@gmail.com>
2024-01-24 17:19:35 +0200

    refact : 01. Band - Main

--- 11. Exams practice/Final Exams/Final Exam Preparation - 24 July 2019/01. Concert/Band.java
+++ 11. Exams practice/Final Exams/Final Exam Preparation - 24 July 2019/01. Concert/Band.java
@@ -1,25 +1,58 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class Band {
-    private String name;
-    private List<String> members;
-    private int playTime;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public Band(String name) {
-        this.name = name;
-        this.members = new ArrayList<>();
-        this.playTime = 0;
+        Map<String, Band> bands = new HashMap<>();
+
+        String input = scanner.nextLine();
+        while (!"start of concert".equals(input)) {
+            String[] tokens = input.split("; ");
+            String command = tokens[0];
+            String name = tokens[1];
+
+            bands.putIfAbsent(name, new Band(name));
+            Band band = bands.get(name);
+
+            switch (command) {
+                case "Add" -> {
+                    String[] members = tokens[2].split(", ");
+                    Arrays.stream(members).forEach(band::add);
+                }
+                case "Play" -> {
+                    int minutes = Integer.parseInt(tokens[2]);
+                    band.play(minutes);
+                }
+            }
+            bands.put(name, band);
+            input = scanner.nextLine();
+        }
+
+        input = scanner.nextLine();
+
+        printResult(input, bands);
     }
 
-    public void add(String name) {
-        if (!this.members.contains(name))
-            this.members.add(name);
+    private static void printResult(String input, Map<String, Band> bands) {
+
+        int totalMinutesPlayed = bands.values().stream().mapToInt(Band::getPlayTime).sum();
+        StringBuilder sb = new StringBuilder(String.format("Total time: %d\n", totalMinutesPlayed));
+
+        bands
+                .values()
+                .stream()
+                .sorted(new ComparatorByMinutesAndName())
+                .forEach(sb::append);
+
+        sb.append(String.format("%s\n",input));
+
+        bands
+                .get(input)
+                .getMembers()
+                .forEach(m ->
+                        sb.append(String.format("=> %s\n", m)));
+
+        System.out.println(sb);
     }
-
-    public void play(int minutes) {
-        playTime += minutes;
-    }
-
-
 }
