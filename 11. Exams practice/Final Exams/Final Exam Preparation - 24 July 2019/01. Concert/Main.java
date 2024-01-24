a11ecf723008839df4d4008698cd402ba1a13a07
wolfie <yavorhristozov@gmail.com>
2024-01-24 11:12:10 +0200

    refact : 01. Concert
    Re-write the task with implementing classes instead of using Maps for keeping band's data.
    This is for practice purpose. For "Judge" testing, please refer to the previous solution (100/100).

--- 11. Exams practice/Final Exams/Final Exam Preparation - 24 July 2019/01. Concert/Main.java
+++ 11. Exams practice/Final Exams/Final Exam Preparation - 24 July 2019/01. Concert/Main.java
@@ -4,75 +4,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, List<String>> bandsMembers = new LinkedHashMap<>();
-        Map<String, Integer> playTime = new LinkedHashMap<>();
+        Map<String, Band> bands = new HashMap<>();
 
         String input = scanner.nextLine();
+
         while (!"start of concert".equals(input)) {
             String[] tokens = input.split("; ");
             String command = tokens[0];
-            String bandName = tokens[1];
+            String name = tokens[1];
+
+            bands.putIfAbsent(name, new Band(name));
+            Band band = bands.get(name);
 
             switch (command) {
-                case "Add":
+                case "Add" -> {
                     String[] members = tokens[2].split(", ");
-                    bandsMembers.putIfAbsent(bandName, new ArrayList<>());
-
-                    List<String> currentMembers = bandsMembers.get(bandName);
-                    for (int i = 0; i < members.length; i++) {
-                        if (!currentMembers.contains(members[i])) {
-                            currentMembers.add(members[i]);
-                        }
-                    }
-                    bandsMembers.put(bandName, currentMembers);
-                    break;
-                case "Play":
-                    int time = Integer.parseInt(tokens[2]);
-                    playTime.putIfAbsent(bandName, 0);
-                    int oldTime = playTime.get(bandName);
-                    playTime.put(bandName, oldTime + time);
-                    break;
+                    Arrays.stream(members).forEach(band::add);
+                }
+                case "Play" -> {
+                    int minutes = Integer.parseInt(tokens[1]);
+                    band.play(minutes);
+                }
             }
+            bands.put(name, band);
             input = scanner.nextLine();
-        }
-
-        int sum = 0;
-        for (Map.Entry<String, Integer> entry : playTime.entrySet()) {
-            sum += entry.getValue();
-        }
-
-        System.out.println(String.format("Total time: %d", sum));
-        playTime
-                .entrySet()
-                .stream()
-                .sorted((b1, b2) -> {
-                    int result = b2.getValue().compareTo(b1.getValue());
-                    if (result == 0) {
-                        result = b1.getKey().compareTo(b2.getKey());
-                    }
-                    return result;
-                }).forEach(e -> {
-            System.out.println(String.format("%s -> %d", e.getKey(), e.getValue()));
-        });
-        String printBand = scanner.nextLine();
-        System.out.println(printBand);
-        List<String> memebers = bandsMembers.get(printBand);
-
-        for (String memeber : memebers) {
-            System.out.println(String.format("=> %s",memeber));
         }
 
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
