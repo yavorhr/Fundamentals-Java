5f7b0386193b735b1a0e25e424369246b19aacb1
wolfie <yavorhristozov@gmail.com>
2024-01-25 11:12:56 +0200

    refact : 02. Song Encryption
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam Preparation - 24 July 2019/02.  Song Encryption/Main.java
+++ 11. Exams practice/Final Exams/Final Exam Preparation - 24 July 2019/02.  Song Encryption/Main.java
@@ -18,44 +18,56 @@ public class Main {
                 String artist = matcher.group("artist");
                 String song = matcher.group("song");
 
-                String result = decypher(artist, song, toDecypher);
-                System.out.println();
+                String result = encryptString(artist, song, toDecypher);
+                printResult("Successful encryption:", result);
+            } else {
+                printResult("Invalid input!");
             }
 
             input = scanner.nextLine();
         }
     }
 
-    private static String decypher(String artist, String song, String toDecypher) {
+    private static void printResult(String... strings) {
+        if (strings.length == 2) {
+            System.out.printf("%s %s\n", strings[0], strings[1]);
+        } else {
+            System.out.println(strings[0]);
+        }
+    }
+
+    private static String encryptString(String artist, String song, String toDecypher) {
         StringBuilder sb = new StringBuilder();
         toDecypher = toDecypher.replace(':', '@');
         int key = artist.length();
 
         for (int i = 0; i < toDecypher.length(); i++) {
             char currentChar = toDecypher.charAt(i);
-            int newLength = currentChar + key;
 
-            if (currentChar != ' ' && currentChar != '\'' && currentChar != '@') {
+            if (currentChar != ' ' && currentChar != '@' && currentChar != '\'') {
+                int newChar = currentChar + key;
 
                 if (Character.isUpperCase(currentChar)) {
-                    if (newLength > 90) {
-                        newChar -= 90;
-                    }
+                    newChar = getNewCharDec(newChar, 90, 64);
                 } else {
-                    if (newChar > 122) {
-                        newChar -= 122;
-                    }
+                    newChar = getNewCharDec(newChar, 122, 96);
                 }
-
-                char newChar = currentChar += key;
-
-
-                sb.append(newChar);
+                sb.append((char) newChar);
             } else {
                 sb.append(currentChar);
             }
         }
+
         return sb.toString();
+    }
+
+    private static int getNewCharDec(int currentCharPlusKey, int upperBound, int lowerBound) {
+        if (currentCharPlusKey > upperBound) {
+            int diff = currentCharPlusKey - upperBound;
+            return lowerBound + diff;
+        } else {
+            return currentCharPlusKey;
+        }
     }
 }
 
