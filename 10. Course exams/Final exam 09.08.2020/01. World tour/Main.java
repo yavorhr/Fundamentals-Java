bdfcee1d694f687d9d1cb88c070b0adfba00fcb4
wolfie <yavorhristozov@gmail.com>
2024-01-25 15:24:19 +0200

    refact : 01. World Tour
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 10. Course exams/Final exam 09.08.2020/01. World tour/Main.java
+++ 10. Course exams/Final exam 09.08.2020/01. World tour/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String initialString = scanner.nextLine();
+        String outputString = scanner.nextLine();
 
         String input = scanner.nextLine();
         while (!"Travel".equals(input)) {
@@ -12,42 +12,49 @@ public class Main {
             String command = tokens[0];
 
             switch (command) {
-                case "Add Stop":
+                case "Add Stop" -> {
                     int index = Integer.parseInt(tokens[1]);
-                    String stringAdd = tokens[2];
-                    StringBuilder sb = new StringBuilder(initialString);
+                    String stringToInsert = tokens[2];
 
-                    if (index >= 0 && index < initialString.length()) {
-                        sb.insert(index, stringAdd);
+                    if (indexIsValid(index, outputString)) {
+                        outputString = new StringBuilder(outputString).insert(index, stringToInsert).toString();
                     }
-                    initialString = sb.toString();
-                    System.out.println(initialString);
-                    break;
+                }
+                case "Remove Stop" -> {
+                    int startIndex = Integer.parseInt(tokens[1]);
+                    int endIndex = Integer.parseInt(tokens[2]);
 
-                case "Remove Stop":
-                    int start = Integer.parseInt(tokens[1]);
-                    int end = Integer.parseInt(tokens[2]);
-
-                    if (start >= 0 && end < initialString.length()) {
-                        String substring = initialString.substring(start, end + 1);
-                        initialString = initialString.replace(substring, "");
+                    if (indexIsValid(startIndex, outputString) && indexIsValid(endIndex, outputString)) {
+                        outputString = new StringBuilder(outputString).delete(startIndex, endIndex + 1).toString();
                     }
-                    System.out.println(initialString);
-                    break;
-
-                case "Switch":
+                }
+                case "Switch" -> {
                     String oldString = tokens[1];
                     String newString = tokens[2];
-                    if (initialString.contains(oldString)) {
-                        initialString = initialString.replace(oldString, newString);
+
+                    if (outputString.contains(oldString)) {
+                        outputString = outputString.replace(oldString, newString);
                     }
-                    System.out.println(initialString);
-                    break;
+                }
             }
+            printResult(outputString);
             input = scanner.nextLine();
         }
-        System.out.println(String.format("Ready for world tour! Planned stops: %s", initialString));
+
+        printResult(String.format("Ready for world tour! Planned stops: %s", outputString));
     }
+
+    private static void printResult(String outputString) {
+        System.out.println(outputString);
+    }
+
+    private static boolean indexIsValid(int index, String outputString) {
+        return index >= 0 && index < outputString.length();
+    }
+
+
 }
+
+
 
 
