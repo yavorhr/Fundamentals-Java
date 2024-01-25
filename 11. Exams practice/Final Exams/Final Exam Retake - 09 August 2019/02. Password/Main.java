c1a0902b69cb047ee7b3ffbb129bc76fbad8d507
wolfie <yavorhristozov@gmail.com>
2024-01-25 13:00:07 +0200

    refact : 02. Password
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams practice/Final Exams/Final Exam Retake - 09 August 2019/02. Password/Main.java
+++ 11. Exams practice/Final Exams/Final Exam Retake - 09 August 2019/02. Password/Main.java
@@ -5,27 +5,41 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
         String regex = "^(.+)>(?<onlyDigits>\\d{3})\\|(?<lowerCases>[a-z]{3})\\|(?<upperCases>[A-Z]{3})\\|(?<anyExcept>[^<>]{3})<\\1$";
+
         Pattern pattern = Pattern.compile(regex);
 
-        int count = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < count; i++) {
+        while (n-- > 0) {
             String input = scanner.nextLine();
             Matcher matcher = pattern.matcher(input);
 
-            if (matcher.find()){
-                String onlyDigits = matcher.group("onlyDigits");
-                String lowerCases = matcher.group("lowerCases");
-                String upperCases = matcher.group("upperCases");
-                String anyExcept = matcher.group("anyExcept");
-                System.out.println(String.format("Password: %s%s%s%s",onlyDigits,lowerCases,upperCases,anyExcept));
+            if (!matcher.find()) {
+                printResult("Try another password!");
             } else {
-                System.out.println("Try another password!");
+                String password = getPassword(matcher);
+                printResult(password);
             }
         }
     }
-}
 
+    private static void printResult(String password) {
+        System.out.println(password);
+    }
+
+    private static String getPassword(Matcher matcher) {
+        StringBuilder sb = new StringBuilder("Password: ");
+
+        String digits = matcher.group("onlyDigits");
+        String lowerCases = matcher.group("lowerCases");
+        String upperCases = matcher.group("upperCases");
+        String symbols = matcher.group("anyExcept");
+
+        sb.append(String.format("%s%s%s%s", digits, lowerCases, upperCases, symbols));
+        return sb.toString();
+    }
+}
 
 
 
