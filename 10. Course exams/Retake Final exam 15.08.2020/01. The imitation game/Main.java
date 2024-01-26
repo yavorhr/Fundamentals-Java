ce36a349c348bfb89ce367c8244e7a8b97472abf
wolfie <yavorhristozov@gmail.com>
2024-01-26 14:54:37 +0200

    refact : 01. The Imitation game
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 10. Course exams/Retake Final exam 15.08.2020/01. The imitation game/Main.java
+++ 10. Course exams/Retake Final exam 15.08.2020/01. The imitation game/Main.java
@@ -4,38 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String initialString = scanner.nextLine();
+        String string = scanner.nextLine();
 
         String input = scanner.nextLine();
         while (!"Decode".equals(input)) {
             String[] tokens = input.split("\\|");
             String command = tokens[0];
-            switch (command) {
-                case "Move":
-                    int count = Integer.parseInt(tokens[1]);
 
-                    String firstLetters = initialString.substring(0, count);
-                    initialString = initialString.replace(firstLetters, "");
-                    StringBuilder sb = new StringBuilder(initialString);
-                    sb.append(firstLetters);
-                    initialString = sb.toString();
-                    break;
-                case "Insert":
+            switch (command) {
+                case "Move" -> {
+                    int numberOfLetters = Integer.parseInt(tokens[1]);
+                    String substring = string.substring(0, numberOfLetters );
+                    string = string
+                            .replace(substring, "")
+                            .concat(substring);
+                }
+                case "Insert" -> {
                     int index = Integer.parseInt(tokens[1]);
                     String value = tokens[2];
-                    StringBuilder newSb = new StringBuilder(initialString);
-                    newSb.insert(index, value);
-                    initialString = newSb.toString();
-                    break;
-                case "ChangeAll":
+                    string = new StringBuilder(string)
+                            .insert(index, value)
+                            .toString();
+
+                }
+                case "ChangeAll" -> {
                     String substring = tokens[1];
                     String replacement = tokens[2];
-                    initialString = initialString.replace(substring, replacement);
-                    break;
+                    string = string.replaceAll(substring, replacement);
+                }
             }
-
             input = scanner.nextLine();
         }
-        System.out.println(String.format("The decrypted message is: %s",initialString));
+
+        System.out.printf("The decrypted message is: %s", string);
     }
 }
