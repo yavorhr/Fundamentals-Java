3daba54c744ed1229f0abc35a18527b2265e2ff3
wolfie <yavorhristozov@gmail.com>
2024-01-26 15:57:04 +0200

    refact : 03. The Pianist
    Change solution by implementing a class MusicPiece which keeps data for a music piece instead of using maps.
    This solution won't work for Judge testing - if you will to test it there, use the first solution  (100/100)

--- 10. Course exams/Retake Final exam 15.08.2020/03. The pianist/Main.java
+++ 10. Course exams/Retake Final exam 15.08.2020/03. The pianist/Main.java
@@ -1,77 +1,85 @@
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, String> pieceComposerMap = new LinkedHashMap<>();
-        Map<String, String> keyAndPieceMap = new LinkedHashMap<>();
-
         int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String[] tokens = scanner.nextLine().split("\\|");
-            String piece = tokens[0];
-            String composer = tokens[1];
-            String key = tokens[2];
+        Map<String, MusicPiece> musicPiecesMap = new HashMap<>();
 
-            pieceComposerMap.putIfAbsent(piece, composer);
-            keyAndPieceMap.putIfAbsent(piece, key);
+        while (n-- > 0) {
+            String[] tokens = scanner.nextLine().split("\\|");
+            addMusicPieceToMap(musicPiecesMap, tokens);
         }
 
         String input = scanner.nextLine();
         while (!"Stop".equals(input)) {
             String[] tokens = input.split("\\|");
             String command = tokens[0];
-            String piece = tokens[1];
+            String name = tokens[1];
 
             switch (command) {
-                case "Add":
+                case "Add" -> {
                     String composer = tokens[2];
                     String key = tokens[3];
-                    if (pieceComposerMap.containsKey(piece) && keyAndPieceMap.containsKey(piece)) {
-                        System.out.println(String.format("%s is already in the collection!", piece));
+                    if (pieceExistsInCollection(musicPiecesMap, name)) {
+                        printOutput(String.format("%s is already in the collection!", name));
                     } else {
-                        pieceComposerMap.put(piece, composer);
-                        keyAndPieceMap.put(piece, key);
-                        System.out.println(String.format("%s by %s in %s added to the collection!", piece, composer, key));
+                        musicPiecesMap.put(name, new MusicPiece(name, composer, key));
+                        printOutput(String.format("%s by %s in %s added to the collection!", name, composer, key));
                     }
-                    break;
-
-                case "Remove":
-                    if (pieceComposerMap.containsKey(piece) && keyAndPieceMap.containsKey(piece)) {
-                        pieceComposerMap.remove(piece);
-                        keyAndPieceMap.remove(piece);
-                        System.out.println(String.format("Successfully removed %s!", piece));
+                }
+                case "Remove" -> {
+                    if (pieceExistsInCollection(musicPiecesMap, name)) {
+                        musicPiecesMap.remove(name);
+                        printOutput(String.format("Successfully removed %s!", name));
                     } else {
-                        System.out.println(String.format("Invalid operation! %s does not exist in the collection.", piece));
+                        printOutput(String.format("Invalid operation! %s does not exist in the collection.", name));
                     }
-                    break;
-
-                case "ChangeKey":
-                    String newKey = tokens[2];
-                    if (pieceComposerMap.containsKey(piece) && keyAndPieceMap.containsKey(piece)) {
-                        keyAndPieceMap.put(piece, newKey);
-                        System.out.println(String.format("Changed the key of %s to %s!", piece, newKey));
+                }
+                case "ChangeKey" -> {
+                    String key = tokens[2];
+                    if (!pieceExistsInCollection(musicPiecesMap, name)) {
+                        printOutput(String.format("Invalid operation! %s does not exist in the collection.", name));
                     } else {
-                        System.out.println(String.format("Invalid operation! %s does not exist in the collection.", piece));
+                        changeKeyOfPiece(musicPiecesMap, name, key);
+                        printOutput(String.format("Changed the key of %s to %s!",name,key));
                     }
-                    break;
+                }
             }
+
             input = scanner.nextLine();
         }
-        pieceComposerMap
-                .entrySet()
+
+        musicPiecesMap.values()
                 .stream()
-                .sorted((p1, p2) -> {
-                    int result = p1.getKey().compareTo(p2.getKey());
-                    if (result == 0) {
-                        result = p1.getValue().compareTo(p2.getValue());
-                        return result;
-                    }
-                    return result;
-                }).forEach(entry -> System.out.println(String.format("%s -> Composer: %s, Key: %s",entry.getKey(),entry.getValue(),keyAndPieceMap.get(entry.getKey()))));
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static void changeKeyOfPiece(Map<String, MusicPiece> musicPiecesMap, String name, String key) {
+        MusicPiece musicPiece = musicPiecesMap.get(name);
+        musicPiece.changeKey(key);
+        musicPiecesMap.put(name, musicPiece);
+    }
+
+    private static boolean pieceExistsInCollection(Map<String, MusicPiece> musicPiecesMap, String name) {
+        return musicPiecesMap.containsKey(name);
+    }
+
+    private static void addMusicPieceToMap(Map<String, MusicPiece> musicPieces, String[] tokens) {
+        String name = tokens[0];
+        String author = tokens[1];
+        String key = tokens[2];
+
+        musicPieces.put(name, new MusicPiece(name, author, key));
+    }
+
+    public static void printOutput(String output) {
+        System.out.println(output);
     }
 }
+
+
 
 
