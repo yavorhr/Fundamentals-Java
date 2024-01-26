691895395f7e7bf31382930ef37ec492135e8eaa
wolfie <yavorhristozov@gmail.com>
2024-01-26 15:13:15 +0200

    refact : 02. Ad astra
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 10. Course exams/Retake Final exam 15.08.2020/02. Ad astra/Main.java
+++ 10. Course exams/Retake Final exam 15.08.2020/02. Ad astra/Main.java
@@ -3,34 +3,56 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    private static final int CAL_PER_DAY_NEEDED = 2000;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-
-        String regexCal = "([\\||#])(?<item>[A-Za-z\\s]+)\\1(?<expiration>\\d{2}\\/\\d{2}\\/\\d{2})\\1(?<calories>\\d*)\\1";
-        Pattern patternCal = Pattern.compile(regexCal);
-        Matcher matcherCal = patternCal.matcher(text);
-
-        int sumCal = 0;
-        while (matcherCal.find()) {
-            int calories = Integer.parseInt(matcherCal.group("calories"));
-            sumCal += calories;
-        }
-
-        int days = sumCal / 2000;
-        System.out.println(String.format("You have food to last you for: %d days!", days));
 
         String regex = "([\\||#])(?<item>[A-Za-z\\s]+)\\1(?<expiration>\\d{2}\\/\\d{2}\\/\\d{2})\\1(?<calories>\\d*)\\1";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(text);
 
+        String input = scanner.nextLine();
+
+        printHowManyDaysOutput(pattern, input);
+        printFoodItems(pattern, input);
+    }
+
+    private static void printHowManyDaysOutput(Pattern pattern, String input) {
+        Matcher matcher = pattern.matcher(input);
+
+        int totalCalories = getTotalCalories(matcher);
+        int days = totalCalories / CAL_PER_DAY_NEEDED;
+
+        if (totalCalories > 0) {
+            System.out.printf("You have food to last you for: %d days\n", days);
+        } else {
+            System.out.println("No matches -> no food");
+        }
+    }
+
+    private static void printFoodItems(Pattern pattern, String input) {
+        Matcher matcher = pattern.matcher(input);
 
         while (matcher.find()) {
             String item = matcher.group("item");
-            String expiration = matcher.group("expiration");
-            int calories = Integer.parseInt(matcher.group("calories"));
-            System.out.println(String.format("Item: %s, Best before: %s, Nutrition: %d", item, expiration, calories));
+            String expirationDate = matcher.group("expiration");
+            String cal = matcher.group("calories");
+
+            System.out.printf("Item: %s, Best before: %s, Nutrition: %s\n", item, expirationDate, cal);
         }
     }
+
+    private static int getTotalCalories(Matcher matcher) {
+        int totalCal = 0;
+        while (matcher.find()) {
+            int cal = Integer.parseInt(matcher.group("calories"));
+            totalCal += cal;
+        }
+
+        return totalCal;
+    }
 }
+
+
+
 
