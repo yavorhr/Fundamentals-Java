c7a495eb3f6910bd057cef4bc1fd1cf5829720aa
wolfie <yavorhristozov@gmail.com>
2024-01-26 14:02:53 +0200

    refact : 03. Numbers
    Solve the task by using Stream API

--- 10. Course exams/Online Mid exam 05.07.2020/03. Numbers/Main.java
+++ 10. Course exams/Online Mid exam 05.07.2020/03. Numbers/Main.java
@@ -1,46 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] numbersAsText = scanner.nextLine().split(" ");
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
 
-        int sum = 0;
-        int currentNum = 0;
-        boolean isFound = false;
+        double average = numbers.stream()
+                .mapToInt(Integer::valueOf)
+                .average()
+                .orElse(0);
 
-        for (int i = 0; i < numbersAsText.length; i++) {
-            int number = Integer.parseInt(numbersAsText[i]);
-            numbers.add(number);
-            sum += number;
-            currentNum++;
-        }
-
-        double averageSum = sum * 1.0 / currentNum;
-
-        Collections.sort(numbers);
-        Collections.reverse(numbers);
-
-        int count = 0;
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) > averageSum) {
-                isFound = true;
-                System.out.print(numbers.get(i) + " ");
-                count++;
-                if (count == 5) {
-                    break;
-                }
-            }
-        }
-
-        if (!isFound) {
-            System.out.println("No");
-        }
+        numbers.stream()
+                .filter(n -> n > average)
+                .sorted((f, s) -> s - f)
+                .limit(5)
+                .forEach(n -> System.out.print(n + " "));
     }
 }
-
